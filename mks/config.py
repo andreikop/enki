0b41c35505abea785822510091806f2bec16be97
hlamer <hlamer@tut.by>
2010-11-18 18:21:22 +0200

    Documentation update

--- mks/config.py
+++
@@ -1,8 +0,0 @@
-"""
-Config file contains some volatile info, such as version, release date and authors
-"""
-
-PACKAGE_NAME = "Monkey Studio IDE"
-PACKAGE_DOMAIN = "monkeystudio.org"
-PACKAGE_VERSION = "3.0.0" 
-PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Filipe AZEVEDO, Andrei KOPATS and The MkS Team"
