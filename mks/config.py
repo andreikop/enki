9982121b1256bc04d70a38b684f18d0f045d1934
hlamer <hlamer@tut.by>
2011-01-26 10:58:54 +0200

    Main window saves and restores its state

--- mks/config.py
+++ mks/config.py
@@ -2,7 +2,8 @@
 Config file contains some volatile info, such as version, release date and authors
 """
 
-PACKAGE_NAME = "Monkey Studio IDE"
-PACKAGE_DOMAIN = "monkeystudio.org"
-PACKAGE_VERSION = "3.0.0" 
+PACKAGE_NAME = "mksv3"
+PACKAGE_ORGANISATION = "MkS Team"
+PACKAGE_DOMAIN = "https://github.com/hlamer/mksv3"
+PACKAGE_VERSION = "0.0.1" 
 PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Filipe AZEVEDO, Andrei KOPATS and The MkS Team"
