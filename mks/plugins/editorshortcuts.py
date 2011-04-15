6ecbcece7ef1aba82c110f267aa973c784890210
Andrei Kopats <hlamer@tut.by>
2011-04-15 09:54:16 +0300

    Editor shortcuts progress

--- mks/plugins/editorshortcuts.py
+++ mks/plugins/editorshortcuts.py
@@ -1,14 +1,115 @@
-from PyQt4.QtCore import Qt, QVariant
-from PyQt4.QtGui import QAbstractItemModel, QDialogButtonBox, QModelIndex
+"""
+editorshortcuts --- Manages QScintilla shortcuts
+================================================
 
-_shortcuts = ((53, "Blablabla", "Ctrl+A",), \
-             )
+Contains editor dialog and functionality for load and save the shortcuts
+"""
+
+import os.path
+
+from PyQt4 import uic
+from PyQt4.QtCore import QAbstractItemModel, QModelIndex, Qt, QObject, QVariant
+from PyQt4.QtGui import QDialog, QDialogButtonBox, QHeaderView
+
+from PyQt4.Qsci import QsciScintilla
+
+from mks.core.core import core, DATA_FILES_PATH
+
+def tr(s):
+    # FIXME add translation
+    return s
+
+_SHORTCUTS = ((QsciScintilla.SCI_LINEDOWNEXTEND, tr("Extend selection down one line")),  
+              (QsciScintilla.SCI_LINEDOWNRECTEXTEND, tr("Extend rectangular selection down one line")),  
+              (QsciScintilla.SCI_LINESCROLLDOWN, tr("Scroll view down one line")),  
+              (QsciScintilla.SCI_LINEUPEXTEND, tr("Extend selection up")),  
+              (QsciScintilla.SCI_LINEUPRECTEXTEND, tr("Extend selection up one line")),  
+              (QsciScintilla.SCI_LINESCROLLUP, tr("Scroll view up one line")),  
+              (QsciScintilla.SCI_PARADOWN, tr("Move down one paragraph")),  
+              (QsciScintilla.SCI_PARADOWNEXTEND, tr("Extend selection up one paragraph")),  
+              (QsciScintilla.SCI_PARAUP, tr("Move down one paragraph")),  
+              (QsciScintilla.SCI_PARAUPEXTEND, tr("Extend selection down one paragraph")),  
+              (QsciScintilla.SCI_CHARLEFTEXTEND, tr("Extend selection left one character")),  
+              (QsciScintilla.SCI_CHARLEFTRECTEXTEND, tr("Extend rectangular selection left one character")),  
+              (QsciScintilla.SCI_CHARRIGHTEXTEND, tr("Extend selection right one character")),  
+              (QsciScintilla.SCI_CHARRIGHTRECTEXTEND, tr("Extend rectangular selection right one character")),  
+              (QsciScintilla.SCI_WORDLEFT, tr("Move left one word")),  
+              (QsciScintilla.SCI_WORDLEFTEXTEND, tr("Extend selection left one word")),  
+              (QsciScintilla.SCI_WORDRIGHT, tr("Move right one word")),  
+              (QsciScintilla.SCI_WORDRIGHTEXTEND, tr("Extend selection right one word")),  
+              (QsciScintilla.SCI_WORDLEFTEND, tr("Move left one word end")),  
+              (QsciScintilla.SCI_WORDLEFTENDEXTEND, tr("Extend selection left one word left")),  
+              (QsciScintilla.SCI_WORDRIGHTEND, tr("Move right one word end")),  
+              (QsciScintilla.SCI_WORDRIGHTENDEXTEND, tr("Extend selection right one word end")),  
+              (QsciScintilla.SCI_WORDPARTLEFT, tr("Move left one word part")),  
+              (QsciScintilla.SCI_WORDPARTLEFTEXTEND, tr("Extend selection left one word part ")),  
+              (QsciScintilla.SCI_WORDPARTRIGHT, tr("Move right one word part")),  
+              (QsciScintilla.SCI_WORDPARTRIGHTEXTEND, tr("Extend selection right one word part")),  
+              (QsciScintilla.SCI_HOME, tr("Move to line start")),  
+              (QsciScintilla.SCI_HOMEEXTEND, tr("Extend selection to line start")),  
+              (QsciScintilla.SCI_HOMERECTEXTEND, tr("Extend rectangular selection to line start")),  
+              (QsciScintilla.SCI_HOMEDISPLAY, tr("Move to start of displayed line")),  
+              (QsciScintilla.SCI_HOMEDISPLAYEXTEND, tr("Extend selection start of displayed line")),  
+              (QsciScintilla.SCI_HOMEWRAP, tr("Home wrap")),  
+              (QsciScintilla.SCI_HOMEWRAPEXTEND, tr("Extend selection on home wrap")),  
+              (QsciScintilla.SCI_VCHOME, tr("Move to firsst VC in line")),  
+              (QsciScintilla.SCI_VCHOMEEXTEND, tr("Extend selection to first VC in line")),  
+              (QsciScintilla.SCI_VCHOMERECTEXTEND, tr("Extend rectangular selection to firts VC in line")),  
+              (QsciScintilla.SCI_VCHOMEWRAP, tr("VC Home wrap")),  
+              (QsciScintilla.SCI_VCHOMEWRAPEXTEND, tr("Extend selection VC Home wrap")),  
+              (QsciScintilla.SCI_LINEEND, tr("Move to end of line")),  
+              (QsciScintilla.SCI_LINEENDEXTEND, tr("Extend selection to end of line")),  
+              (QsciScintilla.SCI_LINEENDRECTEXTEND, tr("Extend rectangular selection to end of line")),  
+              (QsciScintilla.SCI_LINEENDDISPLAY, tr("Move to end displayed line")),  
+              (QsciScintilla.SCI_LINEENDDISPLAYEXTEND, tr("Extend selection to end of displayed line")),  
+              (QsciScintilla.SCI_LINEENDWRAP, tr("Move to line end wrap")),  
+              (QsciScintilla.SCI_LINEENDWRAPEXTEND, tr("Extend selection to line end wrap")),  
+              (QsciScintilla.SCI_DOCUMENTSTART, tr("Move to document start")),  
+              (QsciScintilla.SCI_DOCUMENTSTARTEXTEND, tr("Extend selection to document start")),  
+              (QsciScintilla.SCI_DOCUMENTEND, tr("Move to document end")),  
+              (QsciScintilla.SCI_DOCUMENTENDEXTEND, tr("Extend selection to document end")),  
+              (QsciScintilla.SCI_PAGEUP, tr("Move up one page")),  
+              (QsciScintilla.SCI_PAGEUPEXTEND, tr("Extend selection up one page")),  
+              (QsciScintilla.SCI_PAGEUPRECTEXTEND, tr("Extend rectangular selection up one page")),  
+              (QsciScintilla.SCI_PAGEDOWN, tr("Move down one page")),  
+              (QsciScintilla.SCI_PAGEDOWNEXTEND, tr("Extend selection down one page")),  
+              (QsciScintilla.SCI_PAGEDOWNRECTEXTEND, tr("Extend rectangular selection down one page")),  
+              (QsciScintilla.SCI_STUTTEREDPAGEUP, tr("Move up one page stuttered")),  
+              (QsciScintilla.SCI_STUTTEREDPAGEUPEXTEND, tr("Extend selection up one page stuttered")),  
+              (QsciScintilla.SCI_STUTTEREDPAGEDOWN, tr("Move down one page stuttered")),  
+              (QsciScintilla.SCI_STUTTEREDPAGEDOWNEXTEND, tr("Extend selection down one page stuttered")),  
+              (QsciScintilla.SCI_DELETEBACKNOTLINE, tr("Backspace not a line")),  
+              (QsciScintilla.SCI_DELWORDLEFT, tr("Delete previous word")),  
+              (QsciScintilla.SCI_DELWORDRIGHT, tr("Delete next word")),  
+              (QsciScintilla.SCI_DELLINELEFT, tr("Delete line tp left")),  
+              (QsciScintilla.SCI_DELLINERIGHT, tr("Delete line to right")),  
+              (QsciScintilla.SCI_LINEDELETE, tr("Delete line")),  
+              (QsciScintilla.SCI_LINECUT, tr("Cut line")),  
+              (QsciScintilla.SCI_LINECOPY, tr("Copy line")),  
+              (QsciScintilla.SCI_LINETRANSPOSE, tr("Swap current and previous line")),  
+              (QsciScintilla.SCI_LINEDUPLICATE, tr("Duplicate line")),  
+              (QsciScintilla.SCI_LOWERCASE, tr("To lower case")),  
+              (QsciScintilla.SCI_UPPERCASE, tr("To upper case")),  
+              (QsciScintilla.SCI_EDITTOGGLEOVERTYPE, tr("Edit toggle over type")),  
+              (QsciScintilla.SCI_FORMFEED, tr("Formfeed")),  
+              (QsciScintilla.SCI_BACKTAB, tr("Delete one indent")),  
+              (QsciScintilla.SCI_SELECTIONDUPLICATE, tr("Duplicate selection")),  
+              (QsciScintilla.SCI_CLEAR, tr("Delete")),  
+              (QsciScintilla.SCI_SELECTALL, tr("Select All")),  
+              (QsciScintilla.SCI_ZOOMIN, tr("Zoom In")),  
+              (QsciScintilla.SCI_ZOOMOUT, tr("Zoom Out")),  
+              (QsciScintilla.SCI_SETZOOM, tr("Set Zoom")),  
+              (QsciScintilla.SCI_MARKERADD, tr("Set bookmark")),  
+              (QsciScintilla.SCI_MARKERDELETEALL, tr( "Delete all bookmarks" )), 
+              (QsciScintilla.SCI_MARKERPREVIOUS, tr( "Previous bookmark" )), 
+              (QsciScintilla.SCI_MARKERNEXT, tr( "Next bookmark" )))
 
 class EditorShortcutsModel(QAbstractItemModel):
     """Class implements list of actions, visible in the tree view
     """
-    def __init__(self, *args):
-        QAbstractItemModel.__init__(self, args)
+    def __init__(self, dialog, *args):
+        QAbstractItemModel.__init__(self, *args)
+        self._dialog = dialog
 
     def columnCount(self, parent ):
         return 3
@@ -16,30 +117,27 @@ class EditorShortcutsModel(QAbstractItemModel):
     def data(self, index, role ):
         if role in (Qt.DisplayRole, Qt.ToolTipRole):
             if index.column() == 0:  # name
-                return _shortcuts[index.row()][index.column()]
+                return _SHORTCUTS[index.row()][1]
             elif index.column() == 1:  # shortcut
-                pass
-                # TODO get current shortcut here
-            elif index.column() == 2:  # default shortcut
-                return _actions[index.row()][index.column()]
+                return self._dialog.shortcut(index)
+            elif index.column() == 2: # default shortcut
+                return self._dialog.defaultShortcut(index)
         return QVariant()
 
-
     def index(self, row, column, parent ):
-        if (not row in range(0, len(_actions))) or \
+        if (not row in range(0, len(_SHORTCUTS))) or \
            (not column in range(0, 3)) or \
-           parent.isValid()
+           parent.isValid():
             return QModelIndex()
         return self.createIndex(row, column)
 
-
-    def parent(self, index ):
+    def parent(self, index):
         return QModelIndex()
 
     def rowCount(self, parent ):
         if parent.isValid():
             return 0
-        return len(_actions)
+        return len(_SHORTCUTS)
 
     def hasChildren(self, parent ):
         if parent.isValid():
@@ -56,91 +154,112 @@ class EditorShortcutsModel(QAbstractItemModel):
                     return self.tr( "Shortcut" )
                 elif section == 2:
                     return self.tr( "Default Shortcut" )
-        return QAbstractItemModel.headerData( section, orientation, role )
+        return QAbstractItemModel.headerData( self, section, orientation, role )
 
 
-class EditorShortcutsEditor(QDialog):
+class EditorShortcutsDialog(QDialog):
     """Dialog for editor shortcuts
     """
     def __init__(self, *args):
         QDialog.__init__(self, *args)
-        self._model = EditorShortcutsModel()
-
-        self.ui = self.ui_pActionsNodeShortcutEditor        
-        self.ui.setupUi( self )
-        self.ui.leFilter.setSearchButtonVisible( False )
-        self.ui.leFilter.setPromptText( tr( "Text filter..." ) )
-        self.ui.tvActions.setModel( mProxy )
-        self.ui.tvActions.header().setResizeMode( 0, QHeaderView.Stretch )
-        self.ui.tvActions.header().setResizeMode( 1, QHeaderView.ResizeToContents )
-        self.ui.tvActions.header().setResizeMode( 2, QHeaderView.ResizeToContents )
-        self.ui.tvActions.expandAll()
+        self._model = EditorShortcutsModel(self)
+        uic.loadUi(os.path.join(DATA_FILES_PATH, 'ui/EditorShortcutsDialog.ui'), self)
+        self.leFilter.setSearchButtonVisible( False )
+        self.leFilter.setPromptText( self.tr( "Text filter..." ) )
+        self.tvActions.setModel( self._model )
+        self.tvActions.header().setResizeMode( 0, QHeaderView.Stretch )
+        self.tvActions.header().setResizeMode( 1, QHeaderView.ResizeToContents )
+        self.tvActions.header().setResizeMode( 2, QHeaderView.ResizeToContents )
+        self.tvActions.expandAll()
 
         # connections
-        self.ui.tvActions.selectionModel().selectionChanged.connect(self.tvActions_selectionModel_selectionChanged)
+        self.tvActions.selectionModel().selectionChanged.connect(self.tvActions_selectionModel_selectionChanged)
         
         self.tvActions_selectionModel_selectionChanged()
 
-    def self.selectedIndex(self):
-        indexes = self.ui.tvActions.selectionModel().selectedIndexes()
+    def selectedIndex(self):
+        """Get index of the selected item
+        """
+        indexes = self.tvActions.selectionModel().selectedIndexes()
         if indexes:
             return indexes[0]
         return QModelIndex()
 
     def setShortcut(self, index, shortcut):
+        """Set shortcut for the action, identified by the index
+        """
         # TODO implement
+        pass
 
     def shortcut(self, index):
+        """Get shortcut for the action, identified by the index
+        """    
         # TODO implement
+        return 'short'
     
     def defaultShortcut(self, index):
+        """Get default shortcut for the action, identified by the index
+        """
         # TODO implement
+        return 'def'
     
     def isDefaultShortcut(self, index):
-        return index.isValid() and self.shortcut(index) != self.defaultShortcut(index)
-    
-    #def on_leFilter_textChanged(self, text ):
-    #    mProxy.setFilterWildcard( text )
-    #    self.ui.tvActions.expandAll()
+        """Check if shortcut for action is default
+        """
+        return index.isValid() and \
+               self.shortcut(index) != self.defaultShortcut(index)
 
     def tvActions_selectionModel_selectionChanged(self):
+        """Selection changed in the list
+        """
         index = self.selectedIndex()
         if index.isValid():
-            self.ui.kseShortcut.setText(index.shortcut())
+            self.kseShortcut.setText(self.shortcut(index))
         else:
-            self.ui.kseShortcut.clear()
+            self.kseShortcut.clear()
         
-        self.ui.kseShortcut.setEnabled(index.isValid())
-        self.ui.tbSet.setEnabled(False)
-        self.ui.tbClear.setEnabled( action and not action.shortcut().isEmpty() )
-        self.ui.dbbButtons.button( QDialogButtonBox.Reset ).setEnabled( False )
-        self.ui.dbbButtons.button( QDialogButtonBox.RestoreDefaults ).setEnabled(not self.isDefaultShortcut(index))
-        self.ui.kseShortcut.setFocus()
+        self.kseShortcut.setEnabled(index.isValid())
+        self.tbSet.setEnabled(False)
+        self.tbClear.setEnabled( index.isValid() and bool(self.shortcut(index)) )
+        self.dbbButtons.button( QDialogButtonBox.Reset ).setEnabled( False )
+        self.dbbButtons.button( QDialogButtonBox.RestoreDefaults ).setEnabled(not self.isDefaultShortcut(index))
+        self.kseShortcut.setFocus()
 
     def on_kseShortcut_textChanged(self, text):
+        """Text has been changed in the shortcut input
+        """
         index = self.selectedIndex()
-        self.ui.tbSet.setEnabled( action and not self.ui.kseShortcut.text().isEmpty() )
-        self.ui.dbbButtons.button( QDialogButtonBox.Reset ).setEnabled( True )
-        self.ui.dbbButtons.button( QDialogButtonBox.RestoreDefaults ).setEnabled(not self.isDefaultShortcut(index))
+        self.tbSet.setEnabled( index.isValid() and bool(self.kseShortcut.text()) )
+        self.dbbButtons.button( QDialogButtonBox.Reset ).setEnabled( True )
+        self.dbbButtons.button( QDialogButtonBox.RestoreDefaults ).setEnabled(not self.isDefaultShortcut(index))
 
     def on_tbSet_clicked(self):
+        """*Set* button has been clicked
+        """
         index = self.selectedIndex()
-        if index.isValid() and not self.ui.kseShortcut.text().isEmpty():
-            self.setShortcut(index, self.ui.kseShortcut.text())
-
+        if index.isValid() and not self.kseShortcut.text().isEmpty():
+            self.setShortcut(index, self.kseShortcut.text())
 
     def on_tbClear_clicked(self):
+        """*Clear* button has been clicked
+        """
         index = self.selectedIndex()
         if index.isValid():
-            self.setShortcut( action, None)
+            self.setShortcut( index, None)
 
     def on_dbbButtons_clicked(self, button ):
-        stButton = self.ui.dbbButtons.standardButton( button )
-        if stButton = QDialogButtonBox.Reset:
+        """One of dialog button box buttons has been clicked
+        """
+        stButton = self.dbbButtons.standardButton( button )
+        if stButton == QDialogButtonBox.Reset:
             self.tvActions_selectionModel_selectionChanged()
-        elif stButton = QDialogButtonBox.RestoreDefaults:
+        elif stButton == QDialogButtonBox.RestoreDefaults:
             index = self.selectedIndex()
             if index.isValid():
                 self.setShortcut(index, self.defaultShortcut(index))
-        elif stButton = QDialogButtonBox.Ok:
+        elif stButton == QDialogButtonBox.Ok:
             self.accept()
+
+    def on_leFilter_textChanged(self, text ):
+        mProxy.setFilterWildcard( text )
+        self.tvActions.expandAll()
