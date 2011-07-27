123c4862d92a1132b9f2627fe7cb5ed8641096f2
Andrei Kopats <hlamer@tut.by>
2011-07-27 10:34:56 +0300

    createConfig function moved out from config to core

--- mks/core/config.py
+++ mks/core/config.py
@@ -12,67 +12,47 @@ instance is accessible as: ::
     from mks.core.core import core
     core.config()
 
-First time created by ::class:mks.core.core.Core
+Created by ::class:mks.core.core.Core
 """
-
-import shutil
-import sys
-import os.path
 
 from mks._3rdparty.configobj import ConfigObj, flatten_errors, ParseError
 from mks._3rdparty.validate import Validator
 
-from mks.core.core import core, DATA_FILES_PATH
-
-_DEFAULT_CONFIG_PATH = os.path.join(DATA_FILES_PATH, 'config/mksv3.default.cfg')
-_DEFAULT_CONFIG_SPEC_PATH = os.path.join(DATA_FILES_PATH, 'config/mksv3.spec.cfg')
-_CONFIG_PATH = os.path.expanduser('~/.mksv3.cfg')
-
-def createConfig():
-    """Open config file and return Config instance
-    
-    Function creates config file in user's home directory, if necessary,
-    validates and opens it.
-    """
-    try:
-        # Create config file in the users home
-        if not os.path.exists(_CONFIG_PATH):
-            try:
-                shutil.copyfile(_DEFAULT_CONFIG_PATH, _CONFIG_PATH)
-            except IOError, ex:
-                raise UserWarning('Failed to create configuration file. Error:\n' + 
-                                  unicode(str(ex), 'utf_8'))
-        # Open config file
-        config = Config(_CONFIG_PATH, configspec=_DEFAULT_CONFIG_SPEC_PATH)
-    except UserWarning, ex:
-        messageString = unicode(str(ex)) + '\n' + 'Using default configuration'
-        print >> sys.stderr, messageString
-        core.messageManager().appendMessage(messageString)
-        
-        config = Config(_DEFAULT_CONFIG_PATH, configspec=_DEFAULT_CONFIG_SPEC_PATH)
-    return config
-
 class Config(ConfigObj):
     """Settings storage
+    Class extends ConfigObj with few methods, usefull for MkS
+    There are 3 instances of this class currently (when the comment is being writen):
+    * Main configuration file
+    * QScintilla shortcuts
+    * QScintilla settings
     
     Use this object as a dictionary for read and write options.
     Example: ::
         font = core.config()["Editor"]["DefaultFont"]  # read option
         core.config()["Editor"]["DefaultFont"] = font  # write option
     
-    You SHOULD flush config, when writing changed settings finished.
+    You SHOULD flush config, when writing changed settings is finished.
+    Example: ::
+        core.config().flush()
     """
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, enableWriting, *args, **kwargs):
+        """If enableWriting is True - object will write settings on flush(), if False - will do nothing
+        """
+        self.enableWriting = enableWriting
+        # Open config file
         try:
             super(Config, self).__init__(*args, **kwargs)
         except ParseError, ex:
-            raise UserWarning('Failed to parse configuration file %s\n'
-                              'Error:\n'
-                              '%s\n'
-                              'Fix the file or delete it.' % (_CONFIG_PATH, unicode(str(ex), 'utf_8')))
-        self._validate()
-    
+            messageString = 'Failed to parse configuration file %s\n' \
+                            'Error:\n' \
+                            '%s\n' \
+                            'Fix the file or delete it.' % (self.filename, unicode(str(ex), 'utf_8'))
+            raise UserWarning(messageString)
+        
+        if self.configspec is not None:
+            self._validate()
+
     def _validate(self):
         """Validate opened config, raise UserWarning if it is invalid
         """
@@ -103,7 +83,8 @@ class Config(ConfigObj):
         TODO replace with file watcher
         """
         super(Config, self).reload()
-        self._validate()
+        if self.configspec is not None:
+            self._validate()
 
     def get(self, name):
         """Get option by slash-separated path
@@ -125,7 +106,7 @@ class Config(ConfigObj):
 
     def flush(self):
         """Flush config to the disk
-        Does nothing, if default config opened (failed to create config in users home directory)
+        Does nothing, if enableWriting is False (probably default config is opened)
         """
-        if self.filename != _DEFAULT_CONFIG_PATH:
+        if self.enableWriting:
             self.write()
