f13f410b54c2ce9c8b60f55db9da7341dfe5bb28
Andrei Kopats <hlamer@tut.by>
2011-08-19 19:59:18 +0300

    Documentation update

--- build/lib.linux-i686-2.6/mks/core/defines.py
+++
@@ -1,9 +0,0 @@
-"""
-File contains some volatile info, such as version, release date and authors
-"""
-
-PACKAGE_NAME = "mksv3"
-PACKAGE_ORGANISATION = "MkS Team"
-PACKAGE_DOMAIN = "https://github.com/hlamer/mksv3"
-PACKAGE_VERSION = "0.0.1" 
-PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Filipe AZEVEDO, Andrei KOPATS and The MkS Team"
