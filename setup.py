eca98a50ee4ae4ffbee1ba5e468e83790d31cb92
Andrei Kopats <hlamer@tut.by>
2011-11-06 01:12:15 +0300

    stdeb config

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ setup(name=PACKAGE_NAME,
         version=PACKAGE_VERSION,
         description='Next generation Unix code editor',
         long_description='TODO write long description',
-        author='Andrei Kopats, Filipe Azevedo',
+        author='Andrei Kopats',
         author_email='hlamer@tut.by',
         url='www.monkeystudio.org',
         download_url='TODO write download URL',
         packages=['mks', 'mks/core', 'mks/plugins', 'mks/resources'],
         package_data={'mks' : ['ui/*.ui', 'config/*.cfg']},
-        scripts=['./mksv3']
+        scripts=['./mksv3'],
+        command_packages='stdeb.command'
         )
 # TODO classifiers!
