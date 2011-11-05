a46dd00eeba79d44ed96493b1e1c4d51ef091fc9
Andrei Kopats <hlamer@tut.by>
2011-11-06 00:19:59 +0300

    Actual information for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
+from mks.core.defines import PACKAGE_NAME, PACKAGE_VERSION 
+
 from distutils.core import setup
 
-setup(name='mksv3',
-            version='1.0',
-            packages=['mks', 'mks/core', 'mks/plugins', 'mks/resources'],
-            package_data={'mks' : ['ui/*.ui', 'config/*.cfg']},
-            scripts=['./mksv3']
-            )
+setup(name=PACKAGE_NAME,
+        version=PACKAGE_VERSION,
+        description='Next generation Unix code editor',
+        long_description='TODO write long description',
+        author='Andrei Kopats, Filipe Azevedo',
+        author_email='hlamer@tut.by',
+        url='www.monkeystudio.org',
+        download_url='TODO write download URL',
+        packages=['mks', 'mks/core', 'mks/plugins', 'mks/resources'],
+        package_data={'mks' : ['ui/*.ui', 'config/*.cfg']},
+        scripts=['./mksv3']
+        )
+# TODO classifiers!
