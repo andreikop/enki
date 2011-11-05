1123280a30098604db4988847581746b446c57ce
Andrei Kopats <hlamer@tut.by>
2011-11-05 20:27:04 +0300

    Attributes to ignore dev* when making release

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 VERSION=`python -c 'import mks.core.defines; print mks.core.defines.PACKAGE_VERSION'`
-git archive --format=tar --prefix=mksv3-${VERSION} HEAD | gzip > mksv3-${VERSION}.tar.gz
+git archive --format=tar --prefix=mksv3-${VERSION}/ HEAD | gzip > mksv3-${VERSION}.tar.gz
