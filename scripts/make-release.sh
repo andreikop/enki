0a5f4b7403d09277d1c37339dc6686d2dcfe81ac
Andrei Kopats <hlamer@tut.by>
2011-11-07 14:08:55 +0300

    Script for making release removed, since distutils can do it

--- scripts/make-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-VERSION=`python -c 'import mks.core.defines; print mks.core.defines.PACKAGE_VERSION'`
-git archive --format=tar --prefix=mksv3-${VERSION}/ HEAD | gzip > mksv3-${VERSION}.tar.gz
