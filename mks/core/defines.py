b7c331ffcc53184da9661a12806ed2e26e7f4c29
Andrei Kopats <hlamer@tut.by>
2011-11-11 19:33:51 +0300

    Added fresh dependency

--- mks/core/defines.py
+++ mks/core/defines.py
@@ -6,7 +6,7 @@ import os.path
 PACKAGE_NAME = "mksv3"
 PACKAGE_ORGANISATION = "MkS Team"
 PACKAGE_DOMAIN = "https://github.com/hlamer/mksv3"
-PACKAGE_VERSION = "0.1a" 
+PACKAGE_VERSION = "0.1.1" 
 PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Fi1ilipe AZEVEDO, Andrei KOPATS and The MkS Team"
 
 CONFIG_DIR = os.path.expanduser('~/.mksv3/')
