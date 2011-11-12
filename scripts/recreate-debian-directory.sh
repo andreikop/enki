3b9f6b5d45dea7896d5d022a848e602cad87c708
Andrei Kopats <hlamer@tut.by>
2011-11-12 11:56:51 +0300

    Debian scripts and files cleanup

---
+++ scripts/recreate-debian-directory.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DEBFULLNAME=`./setup.py --author`
+export DEBEMAIL=`./setup.py --author-email`
+VERSION=`./setup.py --version`
+LICENSE=`./setup.py --license`
+PACKAGE_NAME=`./setup.py --name`
+ARCHIVE=dist/${PACKAGE_NAME}-${VERSION}.tar.gz
+BUILD_DIR=${PACKAGE_NAME}-${VERSION}
+
+./setup.py sdist
+
+rm -rf build
+mkdir build
+cd build
+tar -xf ../${ARCHIVE}
+cd ${BUILD_DIR}
+
+dh_make \
+    --file=../../${ARCHIVE} \
+    --copyright=${LICENSE} \
+    --single \
+    --createorig
+
+#cd debian && rm *.ex *.EX README.Debian README.source && cd -
+#cp ../../debian/* debian
+#echo '2.7-' > debian/pyversions
