2a1e4554547925fb4665597c9aef7c7a4292590f
Andrei Kopats <hlamer@tut.by>
2011-11-20 20:08:41 +0300

    Home page progress

--- mks/core/defines.py
+++ mks/core/defines.py
@@ -7,6 +7,6 @@ PACKAGE_NAME = "mksv3"
 PACKAGE_ORGANISATION = "MkS Team"
 PACKAGE_URL = "https://github.com/hlamer/mksv3"
 PACKAGE_VERSION = "0.1.3"
-PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Fi1ilipe AZEVEDO, Andrei KOPATS and The MkS Team"
+PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Filipe AZEVEDO, Andrei KOPATS and The MkS Team"
 
 CONFIG_DIR = os.path.expanduser('~/.mksv3/')
