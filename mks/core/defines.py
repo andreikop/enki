25c0f7c9d2b97cd6a519b24e542cbf776655bfee
Andrei Kopats <hlamer@tut.by>
2011-11-20 23:19:04 +0300

    Version 0.1.5

--- mks/core/defines.py
+++ mks/core/defines.py
@@ -6,7 +6,7 @@ import os.path
 PACKAGE_NAME = "mksv3"
 PACKAGE_ORGANISATION = "MkS Team"
 PACKAGE_URL = "https://github.com/hlamer/mksv3"
-PACKAGE_VERSION = "0.1.4"
+PACKAGE_VERSION = "0.1.5"
 PACKAGE_COPYRIGHTS = "(C) 2005 - 2010 Filipe AZEVEDO, Andrei KOPATS and The MkS Team"
 
 CONFIG_DIR = os.path.expanduser('~/.mksv3/')
