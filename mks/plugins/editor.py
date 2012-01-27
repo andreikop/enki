78d970ef14e5d7ad9989d5922b794a7f6112ddf4
Andrei Kopats <hlamer@tut.by>
2012-01-27 18:21:15 +0300

    Cosmetic fixes. New bugs

--- mks/plugins/editor.py
+++ mks/plugins/editor.py
@@ -279,6 +279,7 @@ class LexerConfig:
                     lexerSection['indentationWarning'] = True
                     lexerSection['indentationWarningReason'] = reasonFromQsci[reason]
 
+
 def _getPygmentsSchemeLexer(editor):
     """Construct and return pygments lexer for Scheme. Sets valid language name, lazy import
     """
@@ -287,7 +288,7 @@ def _getPygmentsSchemeLexer(editor):
     except ImportError:
         QMessageBox.critical(core.workspace(), "Failed to load pygments",
                              "<html>mksv3 can't highlight Scheme source, "\
-                             "because <b>pygments</b> library not found</html>")
+                             "because <b>pygments</b> package not found</html>")
         return None
     return mks.plugins.lexerpygments.LexerPygments(editor, 'Scheme')
 
