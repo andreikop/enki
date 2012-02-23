dd335d3bddecc2d57d498ee6e24355df4ff15cfb
Andrei Kopats <hlamer@tut.by>
2012-02-23 08:26:25 +0300

    Removed some actions, which are not widelly used

--- mks/plugins/editor/shortcuts.py
+++ mks/plugins/editor/shortcuts.py
@@ -35,18 +35,10 @@ _ACTIONS = (\
 (qsci.SCI_DOCUMENTENDEXTEND, 'mEdit/mSelection/mDocument/aToEnd', tr('Extend to end'), 'Ctrl+Alt+End', ''),
 \
 (qsci.SCI_PAGEDOWNEXTEND, 'mEdit/mSelection/mPage/aDownOnePage', tr('Down'), 'Shift+PgDown', ''),
-(qsci.SCI_STUTTEREDPAGEUPEXTEND, 'mEdit/mSelection/mPage/aUpOnePageStuttered', tr('Up stuttered'), '', ''),
 (qsci.SCI_PAGEUPEXTEND, 'mEdit/mSelection/mPage/aUpOnePage', tr('Up'), 'Shift+PgUp', ''),
-(qsci.SCI_STUTTEREDPAGEDOWNEXTEND, 'mEdit/mSelection/mPage/aDownOnePageStuttered', tr('Down stuttered'), '', ''),
 \
 (qsci.SCI_HOMEEXTEND, 'mEdit/mSelection/mLine/aStart', tr('To start'), '', ''),
-(qsci.SCI_HOMEDISPLAYEXTEND, 'mEdit/mSelection/mLine/aStartOfDisplayed', tr('To start of displayed'), '', ''),
-(qsci.SCI_HOMEWRAPEXTEND, 'mEdit/mSelection/mLine/aOnHomeWrap', tr('Home wrap'), '', ''),
-(qsci.SCI_VCHOMEWRAPEXTEND, 'mEdit/mSelection/mLine/aVCHomeWrap', tr('Visible character Home wrap'), '', ''),
-(qsci.SCI_VCHOMEEXTEND, 'mEdit/mSelection/mLine/aToFirstVC', tr('First visible character'), 'Shift+Home', ''),
 (qsci.SCI_LINEENDEXTEND, 'mEdit/mSelection/mLine/aToEnd', tr('End'), 'Shift+End', ''),
-(qsci.SCI_LINEENDDISPLAYEXTEND, 'mEdit/mSelection/mLine/aToEndOfDisplayed', tr('End of displayed'), '', ''),
-(qsci.SCI_LINEENDWRAPEXTEND, 'mEdit/mSelection/mLine/aEndWrap', tr('End wrap'), '', ''),
 \
 (qsci.SCI_PARAUPEXTEND, 'mEdit/mSelection/mParagraph/aUp', tr('Up'), 'Ctrl+Alt+[', ''),
 (qsci.SCI_PARADOWNEXTEND, 'mEdit/mSelection/mParagraph/aDown', tr('Down'), 'Ctrl+Alt+]', ''),
@@ -55,8 +47,6 @@ _ACTIONS = (\
 (qsci.SCI_WORDRIGHTEXTEND, 'mEdit/mSelection/mWord/aRight', tr('Right'), 'Ctrl+Shift+Right', ''),
 (qsci.SCI_WORDLEFTENDEXTEND, 'mEdit/mSelection/mWord/aLeftEnd', tr('Left end'), '', ''),
 (qsci.SCI_WORDRIGHTENDEXTEND, 'mEdit/mSelection/mWord/aRightEnd', tr('Right end'), '', ''),
-(qsci.SCI_WORDPARTLEFTEXTEND, 'mEdit/mSelection/mWord/aLeftPart', tr('Left part '), 'Ctrl+Alt+/', ''),
-(qsci.SCI_WORDPARTRIGHTEXTEND, 'mEdit/mSelection/mWord/aRightPart', tr('Right part'), 'Ctrl+Alt+\\', ''),
 \
 (qsci.SCI_LINEDOWNRECTEXTEND, 'mEdit/mSelection/mRectangular/aDownOneLine', tr('Down one line'), 'Alt+Shift+Down', ''),
 (qsci.SCI_CHARLEFTRECTEXTEND, 'mEdit/mSelection/mRectangular/aLeftOneCharacter', 
@@ -64,8 +54,6 @@ _ACTIONS = (\
 (qsci.SCI_CHARRIGHTRECTEXTEND, 'mEdit/mSelection/mRectangular/aRightOneCharacter',
                                                     tr('Right one character'), 'Alt+Shift+Right', ''),
 (qsci.SCI_HOMERECTEXTEND, 'mEdit/mSelection/mRectangular/aToLineStart', tr('Line start'), '', ''),
-(qsci.SCI_VCHOMERECTEXTEND, 'mEdit/mSelection/mRectangular/aToFirtsVCInLine',
-                                                    tr('First visible character in line'), 'Alt+Shift+Home', ''),
 (qsci.SCI_LINEENDRECTEXTEND, 'mEdit/mSelection/mRectangular/aToEndOfLine', tr('End of line'), 'Alt+Shift+End', ''),
 (qsci.SCI_PAGEDOWNRECTEXTEND, 'mEdit/mSelection/mRectangular/aDownOnePage',
                                                     tr('Down one page'), 'Alt+Shift+PgDown', ''),
@@ -77,31 +65,19 @@ _ACTIONS = (\
 (qsci.SCI_CHARLEFT, 'mNavigation/mMove/aLeft', tr('Left'), '', ''),
 \
 (qsci.SCI_HOME, 'mNavigation/mMove/mLine/aStart', tr('Start'), '', ''),
-(qsci.SCI_HOMEDISPLAY, 'mNavigation/mMove/mLine/aStartOfDisplayed', tr('Start of displayed'), 'Alt+Home', ''),
-(qsci.SCI_HOMEWRAP, 'mNavigation/mMove/mLine/aHomeWrap', tr('Home wrap'), '', ''),
-(qsci.SCI_VCHOME, 'mNavigation/mMove/mLine/aFirsstVCInLine', tr('First visible character'), '', ''),
-(qsci.SCI_VCHOMEWRAP, 'mNavigation/mMove/mLine/aVCHomeWrap', tr('Visible character home wrap'), '', ''),
 (qsci.SCI_LINEEND, 'mNavigation/mMove/mLine/aToEndOfLine', tr('End'), '', ''),
-(qsci.SCI_LINEENDDISPLAY, 'mNavigation/mMove/mLine/aToEndDisplayedLine', tr('End of displayed'), 'Alt+End', ''),
-(qsci.SCI_LINEENDWRAP, 'mNavigation/mMove/mLine/aEndWrap', tr('End wrap'), '', ''),
 \
 (qsci.SCI_DOCUMENTSTART, 'mNavigation/mMove/mDocument/aStart', tr('Start'), 'Ctrl+Home', ''),
 (qsci.SCI_DOCUMENTEND, 'mNavigation/mMove/mDocument/aEnd', tr('End'), 'Ctrl+End', ''),
 \
 (qsci.SCI_PAGEUP, 'mNavigation/mMove/mPage/aUp', tr('Up'), '', ''),
 (qsci.SCI_PAGEDOWN, 'mNavigation/mMove/mPage/aDown', tr('Down'), '', ''),
-(qsci.SCI_STUTTEREDPAGEUP, 'mNavigation/mMove/mPage/aUpStuttered', tr('Up stuttered'), '', ''),
-(qsci.SCI_STUTTEREDPAGEDOWN, 'mNavigation/mMove/mPage/aDownStuttered', tr('Down stuttered'), '', ''),
 \
 (qsci.SCI_PARAUP, 'mNavigation/mMove/mParagraph/aUp', tr('Up'), 'Ctrl+[', ''),
 (qsci.SCI_PARADOWN, 'mNavigation/mMove/mParagraph/aDown', tr('Down'), 'Ctrl+]', ''),
 \
 (qsci.SCI_WORDLEFT, 'mNavigation/mMove/mWord/aLeft', tr('Left'), 'Ctrl+Left', ''),
 (qsci.SCI_WORDRIGHT, 'mNavigation/mMove/mWord/aRight', tr('Right'), 'Ctrl+Right', ''),
-(qsci.SCI_WORDLEFTEND, 'mNavigation/mMove/mWord/aLeftEnd', tr('Left end'), '', ''),
-(qsci.SCI_WORDRIGHTEND, 'mNavigation/mMove/mWord/aRightEnd', tr('Right end'), '', ''),
-(qsci.SCI_WORDPARTLEFT, 'mNavigation/mMove/mWord/aLeftPart', tr('Left part'), 'Ctrl+/', ''),
-(qsci.SCI_WORDPARTRIGHT, 'mNavigation/mMove/mWord/aRightPart', tr('Right part'), 'Ctrl+\\', ''),
 \
 (qsci.SCI_UNDO, 'mEdit/mHistory/aUndo', tr('Undo'), 'Ctrl+Z', 'undo.png'),
 (qsci.SCI_REDO, 'mEdit/mHistory/aRedo', tr('Redo'), 'Ctrl+Y', 'redo.png'),
@@ -113,15 +89,12 @@ _ACTIONS = (\
 (qsci.SCI_EDITTOGGLEOVERTYPE, 'mEdit/aEditToggleOverType', tr('Toggle over type'), 'Ins', ''),
 \
 (qsci.SCI_TAB, 'mEdit/mInsert/aIndent', tr('Indent'), '', ''),
+(qsci.SCI_BACKTAB, 'mEdit/mDelete/aOneIndent', tr('Delete one indent'), 'Shift+Tab', ''),
 \
 (qsci.SCI_CLEAR, 'mEdit/mDelete/aDelete', tr('Delete'), 'Del', ''),
 (qsci.SCI_DELETEBACK, 'mEdit/mDelete/aBackspace', tr('Backspace'), '', ''),
-(qsci.SCI_BACKTAB, 'mEdit/mDelete/aOneIndent', tr('Delete one indent'), 'Shift+Tab', ''),
 (qsci.SCI_DELWORDLEFT, 'mEdit/mDelete/aPreviousWord', tr('Previous word'), 'Ctrl+Backspace', ''),
 (qsci.SCI_DELWORDRIGHT, 'mEdit/mDelete/aNextWord', tr('Next word'), 'Ctrl+Del', ''),
-(qsci.SCI_LINEDELETE, 'mEdit/mDelete/aLine', tr('Line'), 'Ctrl+Alt+L', ''),
-(qsci.SCI_DELLINELEFT, 'mEdit/mDelete/aLineTpLeft', tr('Line to left'), 'Ctrl+Alt+Backspace', ''),
-(qsci.SCI_DELLINERIGHT, 'mEdit/mDelete/aLineToRight', tr('Line to right'), 'Ctrl+Alt+Del', ''),
 \
 (qsci.SCI_COPY, 'mEdit/mCopyPaste/aCopy', tr('Copy'), 'Ctrl+C', 'copy.png'),
 (MKS_PASTE, 'mEdit/mCopyPaste/aPaste', tr('Paste'), 'Ctrl+V', 'paste.png'),
@@ -132,7 +105,7 @@ _ACTIONS = (\
 \
 (qsci.SCI_ZOOMIN, 'mView/mZoom/aZoomIn', tr('Zoom In'), 'Ctrl++', ''),
 (qsci.SCI_ZOOMOUT, 'mView/mZoom/aZoomOut', tr('Zoom Out'), 'Ctrl+-', ''),
-(qsci.SCI_SETZOOM, 'mView/mZoom/aSetZoom', tr('Reset Zoom'), 'Ctrl+/', ''),
+\
 (MKS_TOGGLE_BOOKMARK, 'mNavigation/mBookmarks/aSetBookmark', tr('Set bookmark'), 'Ctrl+B', ''),
 (MKS_NEXT_BOOKMARK, 'mNavigation/mBookmarks/aPreviousBookmark', tr('Next bookmark'), 'Alt+Down', ''),
 (MKS_PREV_BOOKMARK, 'mNavigation/mBookmarks/aNextBookmark', tr('Previous bookmark'), 'Alt+Up', ''),
