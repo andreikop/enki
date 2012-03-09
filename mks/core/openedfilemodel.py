2fc66fa00fe43134d01df554cbda9bc3a30ea11d
Andrei Kopats <hlamer@tut.by>
2012-03-09 15:40:20 +0300

    Dont use core to get reference to the workspace

--- mks/core/openedfilemodel.py
+++ mks/core/openedfilemodel.py
@@ -75,10 +75,10 @@ class _OpenedFileModel(QAbstractItemModel):
     def __init__(self, parentObject):
         QAbstractItemModel.__init__(self, parentObject )
         self._sortMode = core.config()["Workspace"]["FileSortMode"]
-        workspace = parentObject.parent()
-        workspace.documentOpened.connect(self._onDocumentOpened)
-        workspace.documentClosed.connect(self._onDocumentClosed)
-        workspace.modifiedChanged.connect(self._onDocumentDataChanged)
+        self._workspace = parentObject.parent()
+        self._workspace.documentOpened.connect(self._onDocumentOpened)
+        self._workspace.documentClosed.connect(self._onDocumentClosed)
+        self._workspace.modifiedChanged.connect(self._onDocumentDataChanged)
     
     def columnCount(self, parent):  # pylint: disable=W0613
         """See QAbstractItemModel documentation"""
@@ -89,14 +89,14 @@ class _OpenedFileModel(QAbstractItemModel):
         if parent.isValid():
             return 0
         else:
-            return len(core.workspace().sortedDocuments)
+            return len(self._workspace.sortedDocuments)
     
     def hasChildren(self, parent ):
         """See QAbstractItemModel documentation"""
         if parent.isValid():
             return False
         else:
-            return (len(core.workspace().sortedDocuments) > 0)
+            return (len(self._workspace.sortedDocuments) > 0)
 
     def headerData(self, section, orientation, role ):
         """See QAbstractItemModel documentation"""
@@ -116,7 +116,7 @@ class _OpenedFileModel(QAbstractItemModel):
         if docPath is None:
             return 'untitled'
         
-        documentPathes = [d.filePath() for d in core.workspace().openedDocuments()]
+        documentPathes = [d.filePath() for d in self._workspace.openedDocuments()]
 
         uniquePath = os.path.basename(docPath)
         leftPath = os.path.dirname(docPath)
@@ -158,10 +158,10 @@ class _OpenedFileModel(QAbstractItemModel):
     
     def index(self, row, column, parent ):
         """See QAbstractItemModel documentation"""
-        if  parent.isValid() or column > 0 or column < 0 or row < 0 or row >= len(core.workspace().sortedDocuments) :
+        if  parent.isValid() or column > 0 or column < 0 or row < 0 or row >= len(self._workspace.sortedDocuments) :
             return QModelIndex()
 
-        return self.createIndex( row, column, core.workspace().sortedDocuments[row] )
+        return self.createIndex( row, column, self._workspace.sortedDocuments[row] )
     
     def parent(self, index ):  # pylint: disable=W0613  
         """See QAbstractItemModel documentation"""
@@ -195,12 +195,12 @@ class _OpenedFileModel(QAbstractItemModel):
         
         fromRow = data.data( self.mimeTypes()[0] ).toInt()[0]
         
-        if  row >= len(core.workspace().sortedDocuments):
+        if  row >= len(self._workspace.sortedDocuments):
             row -= 1
         elif  fromRow < row :
             row -= 1
 
-        newDocuments = copy.copy(core.workspace().sortedDocuments)
+        newDocuments = copy.copy(self._workspace.sortedDocuments)
         
         item = newDocuments.pop(fromRow)
         
@@ -209,7 +209,7 @@ class _OpenedFileModel(QAbstractItemModel):
         
         newDocuments.insert(row, item)
         
-        self.rebuildMapping( core.workspace().sortedDocuments, newDocuments )
+        self.rebuildMapping( self._workspace.sortedDocuments, newDocuments )
         
         if  self._sortMode != _OpenedFileModel.Custom :
             self.setSortMode( _OpenedFileModel.Custom )
@@ -227,7 +227,7 @@ class _OpenedFileModel(QAbstractItemModel):
     
     def documentIndex(self, document):
         """Get model index by document"""
-        row = core.workspace().sortedDocuments.index( document )
+        row = self._workspace.sortedDocuments.index( document )
         
         if  row != -1 :
             return self.createIndex( row, 0, document )
@@ -250,11 +250,11 @@ class _OpenedFileModel(QAbstractItemModel):
 
     def sortDocuments(self):
         """Sort documents list according to current sort mode"""
-        newDocuments = copy.copy(core.workspace().sortedDocuments)
+        newDocuments = copy.copy(self._workspace.sortedDocuments)
         
         if self._sortMode == self.OpeningOrder:
-            newDocuments.sort(lambda a, b: cmp(core.workspace().sortedDocuments.index(a), \
-                                               core.workspace().sortedDocuments.index(b)))
+            newDocuments.sort(lambda a, b: cmp(self._workspace.sortedDocuments.index(a), \
+                                               self._workspace.sortedDocuments.index(b)))
         elif self._sortMode == self.FileName:
             newDocuments.sort(lambda a, b: cmp(a.fileName(), b.fileName()))
         elif self._sortMode == self.URL:
@@ -270,7 +270,7 @@ class _OpenedFileModel(QAbstractItemModel):
             pass
         else:
             assert(0)
-        self.rebuildMapping( core.workspace().sortedDocuments, newDocuments )
+        self.rebuildMapping( self._workspace.sortedDocuments, newDocuments )
         # scroll the view
         selected = QObject.parent(self).tvFiles.selectionModel().selectedIndexes()
         if selected:
@@ -291,13 +291,13 @@ class _OpenedFileModel(QAbstractItemModel):
             documentsMapping[ row ] = oldList[row]
             mapping[ row ] = row
 
-        core.workspace().sortedDocuments = newList
+        self._workspace.sortedDocuments = newList
         
         # build mapping
         for pIndex in pOldIndexes:
             row = pIndex.row()
             document = documentsMapping[ row ]
-            index = core.workspace().sortedDocuments.index( document )
+            index = self._workspace.sortedDocuments.index( document )
             mapping[ row ] = index
         
         for pIndex in pOldIndexes:
@@ -305,7 +305,7 @@ class _OpenedFileModel(QAbstractItemModel):
             index = mapping[ row ]
             
             if  pIndex.isValid():
-                pIndexes.append(self.createIndex( index, pIndex.column(), core.workspace().sortedDocuments[index] ))
+                pIndexes.append(self.createIndex( index, pIndex.column(), self._workspace.sortedDocuments[index] ))
             else:
                 pIndexes.append(QModelIndex())
         
@@ -315,8 +315,8 @@ class _OpenedFileModel(QAbstractItemModel):
     def _onDocumentOpened(self, document ):
         """New document opened at workspace. Handle it
         """
-        assert( not document in core.workspace().sortedDocuments )
-        core.workspace().sortedDocuments.append( document )
+        assert( not document in self._workspace.sortedDocuments )
+        self._workspace.sortedDocuments.append( document )
         self.sortDocuments()
         document.documentDataChanged.connect(self._onDocumentDataChanged)
 
@@ -334,7 +334,7 @@ class _OpenedFileModel(QAbstractItemModel):
     def _onDocumentClosed(self, document ):
         """Document has been closed. Unhandle it
         """
-        index = core.workspace().sortedDocuments.index( document )
+        index = self._workspace.sortedDocuments.index( document )
         
         if  index == -1 :
             return
@@ -342,7 +342,7 @@ class _OpenedFileModel(QAbstractItemModel):
         # scroll the view
         QObject.parent(self).startModifyModel()
         self.beginRemoveRows( QModelIndex(), index, index )
-        core.workspace().sortedDocuments.remove( document )
+        self._workspace.sortedDocuments.remove( document )
         self.endRemoveRows()
         QObject.parent(self).finishModifyModel()
 
@@ -354,6 +354,7 @@ class OpenedFileExplorer(pDockWidget):
     """
     def __init__(self, workspace):
         pDockWidget.__init__(self, workspace)
+        self._workspace = workspace
         self.setObjectName("OpenedFileExplorer")
         self.setWindowTitle("&Opened Files")
         self.setWindowIcon(QIcon(":/mksicons/filtered.png"))
@@ -378,7 +379,7 @@ class OpenedFileExplorer(pDockWidget):
         self.tvFiles.setAttribute( Qt.WA_MacShowFocusRect, False )
         self.tvFiles.setAttribute( Qt.WA_MacSmallSize )
 
-        workspace.currentDocumentChanged.connect(self._onCurrentDocumentChanged)
+        self._workspace.currentDocumentChanged.connect(self._onCurrentDocumentChanged)
         
         # disconnected by startModifyModel()
         self.tvFiles.selectionModel().selectionChanged.connect(self._onSelectionModelSelectionChanged)
@@ -431,8 +432,8 @@ class OpenedFileExplorer(pDockWidget):
         focusWidget = self.window().focusWidget()
 
         # set current document
-        document = core.workspace().sortedDocuments[index.row()]
-        core.workspace().setCurrentDocument( document )
+        document = self._workspace.sortedDocuments[index.row()]
+        self._workspace.setCurrentDocument( document )
         
         # restore focus widget
         if  focusWidget :
