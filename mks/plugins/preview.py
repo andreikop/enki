ab9c79ce1f603a7dce034282a473743e74ec0c58
Andrei Kopats <hlamer@tut.by>
2012-05-15 19:38:18 +0300

    Clear preview when last file closed

--- mks/plugins/preview.py
+++ mks/plugins/preview.py
@@ -13,6 +13,27 @@ from mks.fresh.dockwidget.pDockWidget import pDockWidget
 
 from threading import Lock
 
+zen = \
+"""Beautiful is better than ugly.<br/>
+Explicit is better than implicit.<br/>
+Simple is better than complex.<br/>
+Complex is better than complicated.<br/>
+Flat is better than nested.<br/>
+Sparse is better than dense.<br/>
+Readability counts.<br/>
+Special cases aren't special enough to break the rules.<br/>
+Although practicality beats purity.<br/>
+Errors should never pass silently.<br/>
+Unless explicitly silenced.<br/>
+In the face of ambiguity, refuse the temptation to guess.<br/>
+There should be one-- and preferably only one --obvious way to do it.<br/>
+Although that way may not be obvious at first unless you're Dutch.<br/>
+Now is better than never.<br/>
+Although never is often better than *right* now.<br/>
+If the implementation is hard to explain, it's a bad idea.<br/>
+If the implementation is easy to explain, it may be a good idea.<br/>
+Namespaces are one honking great idea -- let's do more of those!"""
+
 class Plugin(QObject):
     """Plugin interface implementation
     """
@@ -177,6 +198,8 @@ class PreviewDock(pDockWidget):
         """
         if new is not None:
             self._thread.process(new.filePath(), new.language(), new.text())
+        else:
+            self._setHtml(None, zen)  # empty dock looks so empty
 
     def _onTextChanged(self, document, text):
         """Text changed, update preview
