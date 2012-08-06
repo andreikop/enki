4b95b5c3b9424ee61d4339081198bb2223a4e9a1
Andrei Kopats <hlamer@tut.by>
2012-08-06 14:12:12 +0300

    Fix sphinx crashes

--- icons/generate-icons-resource.sh
+++ icons/generate-icons-resource.sh
@@ -3,12 +3,18 @@
 QRC_FILE=enkiicons.qrc
 RESOURCE_FILE=../enki/resources/icons.py
 
+TMP_RESOURCE_FILE=/tmp/icons.py
+
 echo '<RCC>' > $QRC_FILE
 echo '	<qresource prefix="/enkiicons" >' >> $QRC_FILE
 for file in `ls -d *.png languages/*.png fresh/*.png logo/* logo/*/*`; do \
 	echo '		<file>'$file'</file>' >> $QRC_FILE;
 done
 echo '	</qresource>' >>$QRC_FILE
-echo '</RCC>' >>$QRC_FILE
+echo '</RCC>' >> $QRC_FILE
 
-pyrcc4 -o $RESOURCE_FILE $QRC_FILE
+pyrcc4 -o $TMP_RESOURCE_FILE $QRC_FILE
+
+grep -v '^qInitResources()' $TMP_RESOURCE_FILE > $RESOURCE_FILE
+
+rm $TMP_RESOURCE_FILE
