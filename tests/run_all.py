902bad5473090930a211746c715674b65c82c4ae
Bryan A. Jones <bjones AT ece.msstate.edu>
2013-12-02 11:00:34 -0600

    Fix: Replace shell script with equivalent Python script.

---
+++ tests/run_all.py
@@ -0,0 +1,14 @@
+#bin/sh
+
+import os
+from os.path import join, getsize
+import subprocess
+
+for root, dirs, files in os.walk('.'):
+    for name in files:
+        full_path = join(root, name)
+        # See if the given file is executable.
+        if full_path.endswith('.py') and os.access(full_path, os.X_OK) and (not full_path.endswith('run_all.py')):
+            print('Running ' + full_path)
+            subprocess.call(['python', full_path])
+    
