dbcec78dc37d5faaa7afa3d98e7024726d23af2a
Bryan A. Jones <bjones AT ece.msstate.edu>
2013-12-02 10:42:58 -0600

    Add: Batch file and Inno Setup script to package the self-contained executable, along with source,        into a Windows installer. Add: GPLv2 license, in HTML (from http://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html),        then translated by Word to an RTF file.

---
+++ win/Enki.iss
@@ -0,0 +1,37 @@
+; .. Copyright (C) 2012-2013 Bryan A. Jones.
+;
+;    This file is part of Enki.
+;
+;    Enki is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+;
+;    Enki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License along with Enki.  If not, see <http://www.gnu.org/licenses/>.
+;
+; *********************************************************************************************************
+; Enki.iss - `Inno Setup <http://www.jrsoftware.org/isinfo.php>`_ Script to package the Enki binary
+; *********************************************************************************************************
+; This produces a setup .exe to install Enki. It was created from Example1.iss which comes with Inno setup plus info from
+; http://opencandy.com/2011/06/09/installer-platform-comparison-making-the-right-choice/.
+;
+; Provide a few handy definitions to avoid repetition.
+#define PRODUCT_NAME 'Enki'
+#define PRODUCT_VERSION GetDateTimeString('ddddd', '', '');
+
+[Setup]
+AppName={#PRODUCT_NAME}
+AppVersion={#PRODUCT_VERSION}
+DefaultDirName={pf}\{#PRODUCT_NAME}
+DefaultGroupName={#PRODUCT_NAME}
+LicenseFile=..\LICENSE.GPL2.rtf
+UninstallDisplayIcon={app}\bin\Enki.exe
+Compression=lzma2
+SolidCompression=yes
+OutputBaseFilename="Install_{#PRODUCT_NAME}"
+OutputDir=.
+
+[Files]
+Source: "..\dist\all\*"; DestDir: "{app}"; Flags: recursesubdirs
+
+[Icons]
+Name: "{group}\{#PRODUCT_NAME}"; Filename: "{app}\bin\Enki.exe"
---
+++ win/build_installer.bat
@@ -0,0 +1,64 @@
+: .. Copyright (C) 2012-2013 Bryan A. Jones.
+:
+:    This file is part of Enki.
+:
+:    Enki is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+:
+:    Enki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+:
+:    You should have received a copy of the GNU General Public License along with Enki.  If not, see <http://www.gnu.org/licenses/>.
+:
+:
+: .. highlight:: BatchLexer
+:
+: ****************************************************************
+: build_dist.bat - Package the executable then publish to the web.
+: ****************************************************************
+: This is the second phase of the build system. It packages a binary executable version of the program, together with its source code, into a single installer.
+:
+: Package creation
+: ================
+: This script makes use of several DOS commands with flags. A quick reference:
+:
+: For ``rmdir``:
+:
+: /S      Removes all directories and files in the specified directory in addition to the directory itself.  Used to remove a directory tree.
+: /Q      Quiet mode, do not ask if ok to remove a directory tree with /S
+:
+: For ``xcopy``:
+:
+: /E           Copies directories and subdirectories, including empty ones.
+: /I           If destination does not exist and copying more than one file, assumes that destination must be a directory.
+:
+: Gather files
+: ------------
+: First, this script gathers the following components needed to create a package, placing everything to be packaged into ``dist/all``.
+:
+: **Note:** this should only by run after a successfull execution of :doc:`build_exe.bat <build_exe.bat>`, which creates the executable. Git should have all changes committed.
+:
+: ==============   ========================   ======================
+: Component        Source                     Dest
+: ==============   ========================   ======================
+: The executable   ``dist/code_chat``         ``dist/all/bin``
+: Source code      Git repo in ``./``            ``dist/all/src``
+: ==============   ========================   ======================
+:
+: Create a clean ``dist/all`` directory and enter it.
+mkdir dist
+cd dist
+rmdir /q /s all
+mkdir all
+cd all
+:
+: Copy over the source code with no intermediate files by cloning the repo then removing the repo files.
+git clone ..\.. src
+rmdir /q /s src\.git
+:
+: Copy over the executable
+xcopy /E /I ..\enki bin
+:
+: Package
+: -------
+: The :doc:`Enki.iss <Enki.iss>` script then packages everything in ``dist/all`` into a single installer.
+cd ..\..
+"\Program Files (x86)\Inno Setup 5\ISCC.exe" win\Enki.iss
