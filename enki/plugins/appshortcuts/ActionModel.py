84086c5777d94383943bd51b4808835e3098fce1
Andrei Kopats <hlamer@tut.by>
2014-02-09 16:42:05 +0300

    Application shortcuts: fix error message text

--- enki/plugins/appshortcuts/ActionModel.py
+++ enki/plugins/appshortcuts/ActionModel.py
@@ -146,7 +146,7 @@ class ActionModel(QAbstractItemModel):
                not actionToCheck.shortcut().isEmpty() and \
                actionToCheck.shortcut() == shortcut :
                 error = tr( "Can't set shortcut, it's already used by action '%s'." % \
-                            self._cleanText( action.text() ))
+                            self._cleanText( actionToCheck.text() ))
                 raise UserWarning(error)
 
         action.setShortcut( shortcut )
