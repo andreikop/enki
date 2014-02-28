c198df6f6edb5bb3b2b820580bf24092f69f1dd5
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-02-28 10:05:41 -0600

    Clean: Whitespace / long lines.

--- tests/test_plugins/test_approx_match.py
+++ tests/test_plugins/test_approx_match.py
@@ -4,11 +4,17 @@
 #
 #    This file is part of Enki.
 #
-#    Enki is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#    Enki is free software: you can redistribute it and/or modify it under the
+#    terms of the GNU General Public License as published by the Free Software
+#    Foundation, either version 3 of the License, or (at your option) any later
+#    version.
 #
-#    Enki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#    Enki is distributed in the hope that it will be useful, but WITHOUT ANY
+#    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License along with Enki.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU General Public License along with
+#    Enki.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ***********************************
 # test_approx_match.py - Unit testing
@@ -73,21 +79,21 @@ class TestApproxMatch(base.TestCase):
                   search_text = 'bqwc?xyzaad',
                   target_text = 'bwxyzcd')
         self.assertEqual(index, 6)
-        
+
     def test_8(self):
         index = f(search_anchor = 6,
                   search_text = 'bwxyzcd',
                   target_text = 'bqwc?xyzaad')
         self.assertIn(index, (9,10))
-        
+
     def test_9(self):
         index = f(search_anchor = 2,
                   search_text = 'abcd',
                   target_text = 'xxabcdabcdabcdxxx')
-        # This is a typical multiple match case. will return a -1        
+        # This is a typical multiple match case. will return a -1
         ##self.assertIn(index, (4,8,12))
         self.assertEqual(index, -1)
-  
+
     # failed test case:
     def test_10(self):
         index = f(search_anchor = 4,
@@ -96,16 +102,16 @@ class TestApproxMatch(base.TestCase):
         # this is a bug i cannot handle using lcs. lcs will find the last abcd, 4 is before the start of the last abcd, so it maps to index 0 rather than 2.
         # the problem happens when map lcs index back to original string. it has multiple matches. the one i choose is not the 'closet' one.
 ##        self.assertEqual(index, 2)
-        
+
     def test_11(self):
         index = f(search_anchor = 57,
                   search_text = 'age = None# `exclude_patterns# <http://sphinx-doc.org/config.html#confval-exclude_patterns>`_: List of# patterns, re',
                   target_text = 'for a list of supported languages.##language = None exclude_patterns: List of patterns, re')
         # ok, this is the second bug i encountered. explaination? check my test. (to put it simple, tre did a bad job)
-        # work around: remove all docutils markup part from search text. remap it back to original text. then use the refined version to do 
+        # work around: remove all docutils markup part from search text. remap it back to original text. then use the refined version to do
         # comparison. get the second mapping. combine these two mapping to get an exact pinpoint location
 ##        self.assertIn(index, range(68,72))
-        
+
 from enki.plugins.preview.approx_match import refine_search_result as lcs
 import copy
 # Given two strings, find their `longest common subsequence <http://en.wikipedia.org/wiki/Longest_common_subsequence_problem>`_. Notice this is different from `longest common substring <http://en.wikipedia.org/wiki/Longest_common_substring_problem>`_.
@@ -122,55 +128,55 @@ class TestLCS(base.TestCase):
                      search_pattern = 'abc',
                      target_substring = '')[2]
         self.assertEqual(string, '')
-        
+
     # identical string match
     def test3(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'abc',
                      target_substring = 'abc')[2]
         self.assertEqual(string, 'abc')
-    
+
     # no match
     def test4(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'Fox',
                      target_substring = 'Bear')[2]
-        self.assertEqual(string, '')    
-        
+        self.assertEqual(string, '')
+
     # unicode test
     def test5(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'Fußball',
                      target_substring = 'Football')[2]
         self.assertEqual(string, 'Fball')
-        
+
     def test6(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'Niederösterreich',
                      target_substring = 'Oberösterreich')[2]
         self.assertEqual(string, 'erösterreich')
-        
+
     # control charater test
     def test7(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'abc\ndef',
                      target_substring = 'gh\nijkl')[2]
         self.assertEqual(string, '\n')
-        
+
     # real test cases. test8 contains long common substring
     def test8(self):
         string = lcs(search_anchor = 0,
                      search_pattern = '# The :doc:`README` user manual gives a broad overview of this system. In contrast, this document discusses the implementation specifics of the CodeChat system.',
                      target_substring = 'The CodeChat user manual gives a broad overview of this system. In contrast, this document discusses the implementation specifics of the CodeChat system.')[2]
         self.assertEqual(string, 'The d user manual gives a broad overview of this system. In contrast, this document discusses the implementation specifics of the CodeChat system.')
-        
+
     # test9 contains mostly short common subseqence fragments. This will cause long editing distance
     def test9(self):
         string = lcs(search_anchor = 0,
                      search_pattern = 'age = None# `exclude_patterns# <http://sphinx-doc.org/config.html#confval-exclude_patterns>`_: List of# patterns, re',
                      target_substring = 'for a list of supported languages.##language = None exclude_patterns: List of patterns, re')[2]
         self.assertEqual(string, 'a  o upte ngg.lnaexclude_patterns: List of patterns, re')
-        
+
 #    # test10 test the performance when comparing two files. this will take about 1min
 #    def test10(self):
 #        print os.getcwd()
@@ -179,7 +185,7 @@ class TestLCS(base.TestCase):
 #        target_substring = copy.deepcopy(search_pattern)
 #        string = lcs(0, search_pattern, target_substring)[2]
 #        self.assertEqual(string, search_pattern)
-      
+
 
 if __name__ == '__main__':
     unittest.main()
