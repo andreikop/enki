c7a9dfa010e0842774835abd4fe1010881f66315
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-07-10 15:34:07 -0500

    Fix: Work around missing os.path.samefile on Windows.

--- tests/test_plugins/test_file_browser.py
+++ tests/test_plugins/test_file_browser.py
@@ -27,7 +27,17 @@ class Test(base.TestCase):
         dock = self.findDock('&File Browser')
         dock._onVisibilityChanged(True)
 
-        self.assertTrue(os.path.samefile(dock.currentPath(), path))
+        # We can't simply do ``dock.currentPath() == path`` because of
+        # Windows fun: ``u'C:/Users/bjones/AppData/Local' != 'c:\\users\\bjones\\appdata\\local'``.
+        # We can't use ``os.path.samefile()`` because that's available only on
+        # Unix for Python 2.7. So, use normpath and normcase per
+        # http://stackoverflow.com/questions/21158667/comparing-two-paths-in-python.
+        self.assertEqual(normpathcase(dock.currentPath()), normpathcase(path))
+
+def normpathcase(path):
+    """A helper function to normalize the path and case of a given path a.
+    """
+    return os.path.normpath(os.path.normcase(path))
 
 
 if __name__ == '__main__':
