96d3172472b6c3e8568b2a74403119c101fe9512
Hejia Pan <hp96@msstate.edu>
2014-07-18 17:07:48 -0500

    Fix: Move all ui check unit tests from test_preview_sync to test_preview.

--- tests/test_plugins/test_preview.py
+++ tests/test_plugins/test_preview.py
@@ -138,38 +138,6 @@ class PreviewTestCase(base.TestCase):
         # be processed further
         return [None if not webViewContent else webViewContent[0][0], logContent]
 
-    def test_html(self):
-        self._doBasicTest('html')
-
-    @requiresModule('docutils')
-    def test_rst(self):
-        self._doBasicTest('rst')
-
-    @requiresModule('markdown')
-    def test_markdown(self):
-        self._doBasicTest('md')
-
-    @requiresModule('markdown')
-    def test_markdown_templates(self):
-        core.config()['Preview']['Template'] = 'WhiteOnBlack'
-        document = self.createFile('test.md', 'foo')
-
-        self._assertHtmlReady(self._showDock)
-        combo = self._widget().cbTemplate
-        self.assertEqual(combo.currentText(), 'WhiteOnBlack')
-        self.assertFalse('body {color: white; background: black;}' in self._visibleText())
-        self.assertTrue('body {color: white; background: black;}' in self._html())
-
-        self._assertHtmlReady(lambda: combo.setCurrentIndex(combo.findText('Default')))
-        self.assertFalse('body {color: white; background: black;}' in self._visibleText())
-        self.assertFalse('body {color: white; background: black;}' in self._html())
-        self.assertEqual(core.config()['Preview']['Template'], 'Default')
-
-        self._assertHtmlReady(lambda: combo.setCurrentIndex(combo.findText('WhiteOnBlack')))
-        self.assertEqual(combo.currentText(), 'WhiteOnBlack')
-        self.assertFalse('body {color: white; background: black;}' in self._visibleText())
-        self.assertTrue('body {color: white; background: black;}' in self._html())
-
 class Test(PreviewTestCase):
     def test_html(self):
         self._doBasicTest('html')
@@ -217,331 +185,13 @@ class Test(PreviewTestCase):
 
         self.openDialog(lambda: combo.setCurrentIndex(combo.count() - 1), inDialog)
 
-    # Web to code sync tests
-    ##----------------------
-    # Test that mouse clicks get turned into a ``jsClick`` signal
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    @requiresModule('docutils')
-    def test_sync1(self):
-        """Test that web-to-text sync occurs on clicks to the web pane.
-        A click at 0, height (top left corner) should produce
-        an index of 0. It doesn't; I'm not sure I understand how
-        the WebView x, y coordinate system works. For now, skip
-        checking the resulting index.
-        """
-        self._doBasicTest('rst')
-        self.assertEmits(
-          lambda: QTest.mouseClick(self._widget().webView,
-            Qt.LeftButton, Qt.NoModifier, QPoint(0, self._widget().webView.height())),
-          self._dock().jsClick,
-          200)
-
-
-    # Test that simulated mouse clicks at beginning/middle/end produce correct ``jsClick`` values
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    def _jsOnClick(self):
-        """Simulate a mouse click by calling ``window.onclick()`` in Javascript."""
-        ret = self._widget().webView.page().mainFrame().evaluateJavaScript('window.onclick()')
-        assert not ret
-
-    def _wsLen(self):
-        """The web text for web-to-text sync will have extra
-        whitespace in it. But ``findText`` operates on a space-less
-        version of the text. Determine how many whitespace characters
-        preceed the text.
-        """
-        wtc = self._dock()._webTextContent()
-        return len(wtc) - len(wtc.lstrip())
-
-
-    def _testSyncString(self, s):
-        """Given a string ``s``, place the cursor after it and simulate a click
-        in the web view. Verify that the index produced by ``jsClick``
-        is correct.
-
-        Params:
-        s - String after which cursor will be placed.
-        """
-        self._doBasicTest('rst')
-        wsLen = self._wsLen()
-        # Choose text to find.
-        ret = self._widget().webView.page().findText(s)
-        assert ret
-        # Now run the Javascript and see if the index with whitespace added matches.
-        self.assertEmits(self._jsOnClick,
-                         self._dock().jsClick,
-                         100,
-                         expectedSignalParams=(len(s) + wsLen,))
-
-    @requiresModule('docutils')
-    def test_sync2a(self):
-        """TODO: simulate a click before the first letter. Select T, then move backwards using
-        https://developer.mozilla.org/en-US/docs/Web/API/Selection.modify.
-        For now, test after the letter T (the first letter).
-        """
-        self._testSyncString('T')
-
-    @requiresModule('docutils')
-    def test_sync2(self):
-        """Simulate a click after 'The pre' and check the resulting ``jsClick`` result."""
-        self._testSyncString('The pre')
-
-    @requiresModule('docutils')
-    def test_sync3(self):
-        """Same as above, but with the entire string."""
-        self._testSyncString(self.testText)
-
-    # Test that sending a ``jsClick`` signal at beginning/middle/end moves cursor in code pane correctly
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    def _sendJsClick(self, index):
-        """Send a ``jsClick`` signal then see if the code view gets sycned correctly.
-
-        Params:
-        index - The index into 'The preview text' string to send and check.
-        """
-        self._doBasicTest('rst')
-        wsLen = self._wsLen()
-        # Move the code cursor somewhere else, rather than index 0,
-        # so working code must change its value.
-        self._dock()._moveTextPaneToIndex(5)
-        assert index != 5
-        # Now, emit the signal for a click a given index into 'The preview text'.
-        self._dock().jsClick.emit(wsLen + index)
-        # Check the new index, which should be 0.
-        p = core.workspace().currentDocument().qutepart.textCursor().position()
-        self.assertEqual(p, index)
-
-    @requiresModule('docutils')
-    def test_sync4(self):
-        """Test a click at the beginning of the string."""
-        self._sendJsClick(0)
-
-    @requiresModule('docutils')
-    def test_sync5(self):
-        """Test a click at the middle of the string."""
-        self._sendJsClick(8)
-
-    @requiresModule('docutils')
-    def test_sync6(self):
-        """Test a click at the end of the string."""
-        self._sendJsClick(len(self.testText))
-
-    # Misc tests
-    ##^^^^^^^^^^
-    @requiresModule('docutils')
-    def test_sync7(self):
-        """Test on an empty document."""
-        self.testText = ''
-        self.test_sync1()
-
-    # Test after the web page was changed and therefore reloaded,
-    # which might remove the JavaScript to respond to clicks.
-    # No test is needed: the previous tests already check this,
-    # since disabling the following lines causes lots of failures::
-    #
-    #   self._widget.webView.page().mainFrame(). \
-    #     javaScriptWindowObjectCleared.connect(self._onJavaScriptCleared)
-
-    @requiresModule('docutils')
-    def test_sync8(self):
-        """Test with javascript disabled."""
-        # The ``_dock()`` method only works after the dock exists.
-        # The method below creates it.
-        self._doBasicTest('rst')
-        self._dock()._onJavaScriptEnabledCheckbox(False)
-        # Click. Nothing will happen, but make sure there's no assertion
-        # or internal error.
-        QTest.mouseClick(self._widget().webView, Qt.LeftButton)
-
-    # Code to web sync tests
-    ##----------------------
-    # Basic text to web sync
-    ##^^^^^^^^^^^^^^^^^^^^^^
-    def _textToWeb(self, s, testText=u'One\n\nTwo\n\nThree', checkText=True):
-        """Move the cursor in the text pane. Make sure it moves
-        to the matching location in the web pane.
-
-        Params:
-        s -  The string in the text pane to click before.
-        testText - The ReST string to use.
-        checkText - True if the text hilighted in the web dock should be
-            compared to the text in s.
-        """
-        # Create multi-line text.
-        self.testText = testText
-        self._doBasicTest('rst')
-        # Find the desired string.
-        index = self.testText.index(s)
-        # The cursor is already at index 0. Moving here
-        # produces no cursorPositionChanged signal.
-        assert index != 0
-        # Move to a location in the first line of the text.
-        # The sync won't happen until the timer expires; wait
-        # for that.
-        self.assertEmits(lambda: self._dock()._moveTextPaneToIndex(index, False),
-          self._dock()._cursorMovementTimer.timeout, 350)
-        # The web view should have the line containing s selected now.
-        if checkText:
-            self.assertTrue(s in self._widget().webView.selectedText())
-
-    @requiresModule('docutils')
-    def test_sync9(self):
-        # Don't use One, which is an index of 0, which causes no
-        # cursor movement and therefore no text to web sync.
-        self._textToWeb('ne')
-
-    @requiresModule('docutils')
-    def test_sync10(self):
-        self._textToWeb('Two')
-
-    @requiresModule('docutils')
-    def test_sync11(self):
-        self._textToWeb('Three')
-
-    # More complex test to web sync
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    @requiresModule('docutils')
-    def test_sync12(self):
-        """Tables with an embedded image cause findText to fail. Make sure no
-        exceptions are raised.
-
-        I can't seem to find a workaround to this: what search text does findText
-        expect when a table + embedded image are involved?
-        """
-        self._textToWeb('table', """
-================  ========================
-header1           header2
-================  ========================
-img               .. image:: img.png
-text after img    text after img
-================  ========================
-
-text after table""", False)
-
-    @requiresModule('docutils')
-    def test_sync14(self):
-        """Tables without an embedded image work just fine.
-        """
-        self._textToWeb('table', """
-================  ========================
-header1           header2
-================  ========================
-img               image:: img.png
-text after img    text after img
-================  ========================
-
-text after table""", True)
-
-    def _row_span_rest(self):
-        return """
-+--------------------------------+-------------+
-| Apple 1                        | Banana 2    |
-+--------------------------------+             |
-| Coco 3                         |             |
-| Cherry 3                       | Bael 2      |
-+--------------------------------+-------------+
-| Text after block 1,2, and 3                  |
-+----------------------------------------------+
-"""
-
-    @requiresModule('docutils')
-    def test_sync15(self):
-        """Text after an image works just fine.
-        """
-        self._textToWeb('table', """
-.. image:: img.png
-
-text after table""", True)
-
-    @requiresModule('docutils')
-    def test_sync16(self):
-        """Tables with column spans produce out-of-order text, so sync in some rows
-        containing a column span fails. The ReST below, copied as text after
-        being redered to HTML, is:
-
-        Apple 1
-        Banana 2
-
-        Bael 2
-        Coco 3 Cherry 3
-        Text after block 1,2, and 3
-
-        Note the reordering: Bael 2 comes before Coco 3 in the plain text, but
-        before it in the ReST. There's not much the approximate match can do to
-        fix this.
-        """
-        self._textToWeb('Banana', self._row_span_rest(), True)
-
-    @requiresModule('docutils')
-    def test_sync17(self):
-        """A failing case of the above test series."""
-        self._textToWeb('Bael', self._row_span_rest(), False)
-
-    @requiresModule('docutils')
-    def test_sync18(self):
-        """Verify that sync after the column span works."""
-        self._textToWeb('Text', self._row_span_rest(), True)
-
-    # Test no sync on closed preview window
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    def test_sync13(self):
-        self._doBasicTest('rst')
-        self._dock().close()
-        # Move the cursor. If there's no crash, we're OK.
-        qp = core.workspace().currentDocument().qutepart
-        cursor = qp.textCursor()
-        cursor.setPosition(1, QTextCursor.MoveAnchor)
-        qp.setTextCursor(cursor)
-
-    # Cases for _alignScrollAmount
-    ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # Case 1: when the source y (in global coordinates) is above the target
-    # window.
-    def test_sync19(self):
-        self._doBasicTest('rst')
-        offset = self._dock()._alignScrollAmount(
-          sourceGlobalTop = 0,
-          sourceCursorTop = 1,
-          targetGlobalTop = 2,
-          targetCursorTop = 1,
-          targetHeight = 4,
-          targetCursorHeight = 1)
-        self.assertEqual(offset, -1)
-
-    # Case 2: when the source y (in global coordinates) is within the target
-    # window.
-    def test_sync20(self):
-        self._doBasicTest('rst')
-        offset = self._dock()._alignScrollAmount(
-          sourceGlobalTop = 0,
-          sourceCursorTop = 2,
-          targetGlobalTop = 0,
-          targetCursorTop = 1,
-          targetHeight = 4,
-          targetCursorHeight = 1)
-        self.assertEqual(offset, 1)
-
-    # Case 3: when the source y (in global coordinates) is belowthe target
-    # window.
-    def test_sync21(self):
-        self._doBasicTest('rst')
-        offset = self._dock()._alignScrollAmount(
-          sourceGlobalTop = 5,
-          sourceCursorTop = 2,
-          targetGlobalTop = 0,
-          targetCursorTop = 1,
-          targetHeight = 4,
-          targetCursorHeight = 1)
-        self.assertEqual(offset, 2)
-
-    # Cases for CodeChat setting ui
-    ##-----------------------------
+    # Cases for literate programming setting ui
+    ##-----------------------------------------
     #
     @requiresModule('CodeChat')
     def test_uiCheck1(self):
         """When Enki runs for the first time, the CodeChat module should be
            disabled by default."""
-        from enki.plugins.preview import SettingsWidget
         sw = SettingsWidget()
         self.assertFalse(sw.cbCodeChatEnable.isChecked())
         self.assertTrue(sw.cbCodeChatEnable.isEnabled())
@@ -714,7 +364,7 @@ content"""
     @requiresModule('CodeChat')
     def test_uiCheck5(self):
         """If Enki is opened without any configuration, the preview dock cannot
-           be found if the opened file is a code file. This will not affect reST
+           be found if the opened file is a code file. This will not affect resT
            files or html files."""
         with self.assertRaises(AssertionError):
             self._doBasicTest('py')
@@ -759,7 +409,6 @@ content"""
         webViewContent, logContent = self._doBasicSphinxTest('rst')
         self.assertTrue(u"doesn't have a title" in logContent)
 
-
     @requiresModule('CodeChat')
     def test_uiCheck7(self):
         """Test that Unicode characters are handled properly.
@@ -781,12 +430,12 @@ content"""
         core.config()['sphinx']['OutputExtension'] = 'html'
 
         self.testText = u"""**********
-österreich
+Енки
 **********
 
 content"""
         webViewContent, logContent = self._doBasicSphinxTest('rst')
-        self.assertTrue(u"<h1>österreich" in webViewContent)
+        self.assertTrue(u"<h1>Енки" in webViewContent)
 
     @requiresModule('CodeChat')
     def test_uiCheck8(self):
@@ -800,7 +449,7 @@ content"""
         core.uiSettingsManager().dialogAccepted.emit();
         self._doBasicTest('py')
 
-    @requiresModule('CodeChat')
+    @base.requiresCmdlineUtility('sphinx-build --version')
     def test_uiCheck9(self):
         """Uninterpretable reStructuredText syntax in source code will generate
            errors and be displayed in the output log window."""
@@ -813,7 +462,7 @@ content"""
         self._doBasicTest('rst')
         self.assertTrue("""Unknown directive type "wrong".""" in self._logText())
 
-    @base.requiresCmdlineUtility('sphinx-build --version')
+    @requiresModule('sphinx')
     def test_uiCheck9a(self):
         """Test sphinx error can be captured correctly"""
         core.config()['sphinx']['Enabled'] = True
@@ -821,7 +470,7 @@ content"""
         core.config()['sphinx']['OutputPath'] = os.path.join(self.TEST_FILE_DIR, '_build\\html')
         core.config()['sphinx']['OutputExtension'] = 'html'
 
-        self.testText = """****
+        self.testText = u"""****
 head3
 ****
 
@@ -846,14 +495,14 @@ content"""
         """Unicode should display correctly in log window too.
         """
         core.config()['CodeChat']['Enabled'] = True
-        self.testText = u'# .. Niederösterreich::'
+        self.testText = u'# .. Енки::'
         self._doBasicTest('py')
-        self.assertTrue(u'Niederösterreich' in self._logText())
-        # do the same test for reStructuredText
-        self.testText = u'.. Niederösterreich::'
+        self.assertTrue(u'Енки' in self._logText())
+        self.testText = u'.. Енки::'
         self._doBasicTest('rst')
-        self.assertTrue(u'Niederösterreich' in self._logText())
+        self.assertTrue(u'Енки' in self._logText())
 
+    @unittest.expectedFailure
     @base.requiresCmdlineUtility('sphinx-build --version')
     def test_uiCheck11a(self):
         """Unicode in log window while in sphinx mode does not work since sphinx
@@ -868,16 +517,14 @@ content"""
 head
 ****
 
-.. Niederösterreich::"""
+.. Енки::"""
         webViewContent, logContent = self._doBasicSphinxTest('rst')
         # Unicode cannot be found in sphinx error message output.
-        self.assertFalse(u'Niederösterreich' in logContent)
-        # Instead, a question mark replaces the unicode.
-        self.assertTrue(u'Nieder?sterreich' in logContent)
+        self.assertTrue(u'Енки' in logContent)
 
     @requiresModule('CodeChat')
     def test_uiCheck12(self):
-        """Test progress bar status (indefinite) when building.
+        """Test progress bar status (indefinitely) when building
         """
         core.config()['CodeChat']['Enabled'] = True
         self.createFile('file.py', self.testText)
@@ -886,10 +533,10 @@ head
 
     @requiresModule('CodeChat')
     def test_uiCheck13(self):
-        """Check different progressbar colors given different scenarios.
+        """Check different progressbar color given different scenarios.
         """
         core.config()['CodeChat']['Enabled'] = True
-        # First, working code with no errors or warnings.
+        # First, First, working code with no errors or warnings.
         self.testText = u'abc'
         self._doBasicTest('rst')
         self.assertEqual(self._widget().prgStatus.styleSheet(), 'QProgressBar::chunk {}')
@@ -949,6 +596,21 @@ head
         self.assertEqual(self._widget().prgStatus.styleSheet(), 'QProgressBar::chunk {}')
         self.assertEqual(self._logText(), '')
 
+    @requiresModule('sphinx')
+    def test_uiCheck18(self):
+        """Basic sphinx test"""
+        core.config()['sphinx']['Enabled'] = True
+        core.config()['sphinx']['ProjectPath'] = self.TEST_FILE_DIR
+        core.config()['sphinx']['OutputPath'] = os.path.join(self.TEST_FILE_DIR, '_build\\html')
+        core.config()['sphinx']['OutputExtension'] = 'html'
+        core.config().flush()
+
+        self.testText = u"""****
+head3
+****
+
+Come and talk"""
+        webViewContent, logContent = self._doBasicSphinxTest('rst')
 
     @base.requiresCmdlineUtility('sphinx-build --version')
     @requiresModule('CodeChat')
@@ -961,7 +623,7 @@ head
         core.config()['sphinx']['OutputPath'] = os.path.join(self.TEST_FILE_DIR, '_build\\html')
         core.config()['sphinx']['OutputExtension'] = 'html'
 
-        self.testText = """# ****
+        self.testText = u"""# ****
 # head
 # ****
 #
