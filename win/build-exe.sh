1f45b400035d8ff68804bae1934b5960c1c40186
Bryan A. Jones <bjones @ ece.msstate.edu>
2014-11-12 21:56:04 -0600

    Fix: Copy ctags to PyInstaller binary.

--- win/build-exe.sh
+++ win/build-exe.sh
@@ -3,5 +3,5 @@ rm -rf build/ dist/
 pyinstaller -y win/enki-sphinx.spec
 mkdir dist/enki/enki/core
 cp -r dist/sphinx-build/* dist/enki
-# To do: copy over ctags, pylint.
+cp /usr/bin/ctags dist/enki
 dist/enki/enki-editor
