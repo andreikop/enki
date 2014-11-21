24ee480485f4adeccffcc823539d4d8caa08bd27
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-11-21 16:40:36 -0600

    Docs: Explain workings of build-exe.sh.

--- win/build-exe.sh
+++ win/build-exe.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
+#
+#
+# ********************************************************************************************
+# build-exe.sh - Shell script to build a Linux binary to Enki and its supporting applications.
+# ********************************************************************************************
+# Prepare
+# =======
+# Scraps from the previous build may disturb the current build. Get rid of it.
 rm -rf build/ dist/
+#
+# Build
+# =====
 pyinstaller -y win/enki-sphinx.spec
+#
+# Combine
+# =======
+# Enki on Linux needs this directory to exist in order to work.
+# It imports things relative to this path, fails if this path
+# doesn't exist.
 mkdir dist/enki/enki/core
 cp -r dist/sphinx-build/* dist/enki
+cp -r dist/pylint/* dist/enki
 cp /usr/bin/ctags dist/enki
+#
+# Test
+# ====
 dist/enki/enki-editor
+#
+# Package
+# =======
 cd dist
 zip -r enki.zip enki
 cd ..
