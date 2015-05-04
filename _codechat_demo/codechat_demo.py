781ae861b4291f1cbd7d027059e85d2370008226
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-05-04 16:23:43 -0500

    Add: A short CodeChat demo Sphinx project.

---
+++ _codechat_demo/codechat_demo.py
@@ -0,0 +1,39 @@
+# .. -*- coding: utf-8 -*-
+#
+# **********************************************
+# codechat_demo.py - Demonstrate CodeChat in use
+# **********************************************
+# `CodeChat <https://bitbucket.org/bjones/documentation/overview>`_ lets you 
+# treat a program as a document, allowing `reStructuredText
+# <http://docutils.sourceforge.net/rst.html>`_ markup in comments. This allows:
+#
+# * Multi-level bulleted lists, 
+#
+#   #. numbered lists, and 
+#
+#      definition
+#         lists.
+# * Creating titles by putting punctuation characters (like \*, the asterisk) 
+#   underneath a section title.
+# * Hyperlinks (see paragraph above).
+# * Figures.
+#
+#   .. image:: ../img/logo.png
+# * Varying fonts, such as ``monospaced``, *italics*, **bold**, 
+#   :sup:`superscript`, and :sub:`subscript`. 
+# * Tables:
+#
+#   =====  =====  =======
+#   A      B      A and B
+#   =====  =====  =======
+#   False  False  False
+#   True   False  False
+#   False  True   False
+#   True   True   True
+#   =====  =====  =======
+#
+# ...and much, much more.
+
+def this_is_a_program(so_source_code, is_nicely_formatted):
+    and_syntax(highlighted)
+    
