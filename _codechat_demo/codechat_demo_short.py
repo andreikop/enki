4e280f0bc0ad36e65bfe7ace2e70abf1bb16905d
Andrei Kopats <hlamer@tut.by>
2015-05-09 20:35:38 +0300

    CodeChat scresnshot wip

---
+++ _codechat_demo/codechat_demo_short.py
@@ -0,0 +1,24 @@
+# .. -*- coding: utf-8 -*-
+#
+# **********************************************
+# CodeChat demo
+# **********************************************
+# `CodeChat
+# <https://bitbucket.org/bjones/documentation/overview>`_
+# lets you treat a program as a document, allowing
+# `reStructuredText
+# <http://docutils.sourceforge.net/rst.html>`_ markup
+# in comments. This allows you to write literate programs
+# which contain:
+#
+# * `Links <http://enki-editor.org>`_
+# * Lists.
+# * **Various** *formatting* ``styles``.
+# * Tables.
+# * Titles
+# * And even figures
+#
+#   .. image:: enki.png
+def this_is_a_program(so):
+    source_code is nicely_formatted and
+    syntax(highlighted)
