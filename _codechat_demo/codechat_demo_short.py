bc1d5332c90dc467a762017bf07a8a1e732ba6ac
Andrei Kopats <hlamer@tut.by>
2015-05-19 18:56:06 +0300

    formatting fixes

--- _codechat_demo/codechat_demo_short.py
+++ _codechat_demo/codechat_demo_short.py
@@ -1,24 +1,26 @@
 # .. -*- coding: utf-8 -*-
 #
+#
 # **********************************************
 # CodeChat demo
 # **********************************************
+#
 # `CodeChat
 # <https://pythonhosted.org/CodeChat/README.html>`_
 # lets you treat a program as a document, allowing
 # `reStructuredText
-# <http://docutils.sourceforge.net/rst.html>`_ markup
-# in comments. This allows you to write literate programs
-# which contain:
+# <http://docutils.sourceforge.net/rst.html>`_
+# markup in comments. This allows you to write
+# literate programs which contain:
 #
 # * `Links <http://enki-editor.org>`_
 # * Lists.
 # * **Various** *formatting* ``styles``.
 # * Tables.
-# * Titles (see above).
 # * And even figures:
 #
 #   .. image:: enki.png
+#
 def this_is_a_program(so):
     source_code = nicely_formatted and \
       "syntax highlighted"
