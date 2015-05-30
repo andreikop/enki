1afeb4f7e059bccfed2d65202aa7659ebc8a9260
Andrei Kopats <hlamer@tut.by>
2015-05-30 22:03:15 +0300

    Version 15.05

--- enki/core/defines.py
+++ enki/core/defines.py
@@ -6,11 +6,11 @@ import os.path
 PACKAGE_NAME = "Enki"
 PACKAGE_ORGANISATION = "Andrei Kopats"
 PACKAGE_URL = "https://enki-editor.org"
-PACKAGE_VERSION = "15.04.0"
+PACKAGE_VERSION = "15.05.0"
 PACKAGE_COPYRIGHTS = "(C) 2015 Andrei KOPATS"
 
 QUTEPART_SUPPORTED_MAJOR = 2
-QUTEPART_SUPPORTED_MINOR = 0
+QUTEPART_SUPPORTED_MINOR = 2
 
 # Choose base config dir according to http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 if os.environ.get('XDG_CONFIG_HOME', ''):  # if set and not empty
