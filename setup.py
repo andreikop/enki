137e75b0f2c3c27bd54f01c3fb5db658476ac7f3
Bryan A. Jones <bjones@ece.msstate.edu>
2017-01-19 12:54:29 -0600

    Fix: Create a cross-platform enki script when installing.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 import sys
 import pkgutil
-import platform
 from distutils.core import setup
 
 
@@ -106,8 +105,6 @@ for loader, name, ispkg in pkgutil.iter_modules(['enki/plugins']):
         package_data['enki'].append('plugins/%s/templates/*' % name)
         package_data['enki'].append('plugins/%s/sphinx_templates/*' % name)
 
-script = 'bin/enki-editor.pyw' if platform.system() == 'Windows' else 'bin/enki'
-
 if __name__ == '__main__':
     """ Dependency checking is disabled for Debian builds
     if 'install' in sys.argv:
@@ -127,7 +124,11 @@ if __name__ == '__main__':
           packages=packages,
           package_dir=package_dir,
           package_data=package_data,
-          scripts=[script],
+          entry_points={
+            'gui_scripts': [
+                ['enki = enki.main:main'],
+            ],
+          },
           data_files=data_files,
           classifiers=classifiers,
           license='gpl2',
