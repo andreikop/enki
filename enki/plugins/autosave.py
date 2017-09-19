9abbe6dd3c9f2ab12d424149b6f1c356463219a6
Marco Laspe <marco@rockiger.com>
2017-09-19 16:07:11 +0200

    Save only if file is writable, else warn the user.

--- enki/plugins/autosave.py
+++ enki/plugins/autosave.py
@@ -2,9 +2,10 @@
 autosave --- saves all files if enki loses focus
 =============================================================
 """
-import inspect
+import os
 
-from PyQt5.QtWidgets import QApplication, QWidget, QCheckBox, QVBoxLayout, QSpacerItem, QSizePolicy, QLabel
+from PyQt5.QtWidgets import (QApplication, QWidget, QCheckBox, QVBoxLayout,
+                             QSpacerItem, QSizePolicy, QLabel)
 
 from enki.core.core import core
 from enki.core.uisettings import CheckableOption
@@ -12,6 +13,9 @@ from enki.core.uisettings import CheckableOption
 # DONE Setting page
 # DONE checkbox if autosave is activated,
 # DONE Cleanup code (Terminate plugin, etc.)
+# DONE Save only if file is changed
+# DONE Save only if file is writable, else warn the user.
+
 
 class SettingsPage(QWidget):
     """Settings page for Autosave plugi"""
@@ -26,7 +30,10 @@ class SettingsPage(QWidget):
         self._layout = QVBoxLayout(self)
         self._layout.addWidget(self._label)
         self._layout.addWidget(self.checkbox)
-        self._layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
+        self._layout.addSpacerItem(QSpacerItem(0, 0,
+                                               QSizePolicy.MinimumExpanding,
+                                               QSizePolicy.MinimumExpanding))
+
 
 class Plugin:
     """Plugin interface implementation
@@ -45,15 +52,17 @@ class Plugin:
 
         self._checkSettings()
 
-        core.uiSettingsManager().aboutToExecute.connect(self._onSettingsDialogAboutToExecute)
+        core.uiSettingsManager().aboutToExecute.connect(
+            self._onSettingsDialogAboutToExecute)
 
     def terminate(self):
         """clean up"""
-        core.uiSettingsManager().aboutToExecute.disconnect(self._onSettingsDialogAboutToExecute)
+        core.uiSettingsManager().aboutToExecute.disconnect(
+            self._onSettingsDialogAboutToExecute)
 
     def _onFocusChanged(self, newWindow):
         """Tests if no window is focused"""
-        if newWindow == None and self._isAutoSaveActive():
+        if newWindow is None and self._isAutoSaveActive():
             self._saveFiles()
 
     def _onClose(self, qobject):
@@ -63,7 +72,13 @@ class Plugin:
     def _saveFiles(self):
         """Saves all open files"""
         for document in core.workspace().documents():
-            document.saveFile()
+            if document.qutepart.document().isModified():
+                if not os.access(document.filePath(), os.W_OK):
+                    core.mainWindow().appendMessage(
+                        "Can not save file '%s', because it is not writable!" %
+                        document.filePath(), 4000)
+                else:
+                    document.saveFile()
 
     def _onSettingsDialogAboutToExecute(self, dialog):
         """UI settings dialogue is about to execute.
@@ -73,13 +88,15 @@ class Plugin:
         dialog.appendPage(u"Autosave", page)
 
         # Options
-        dialog.appendOption(CheckableOption(dialog, core.config(), "Autosave/Active", page.checkbox))
+        dialog.appendOption(CheckableOption(dialog, core.config(),
+                                            "Autosave/Active",
+                                            page.checkbox))
 
     def _checkSettings(self):
         """Check if settings are present in the core configuration file,
         else create and return them.
         """
-        if not "Autosave" in core.config():
+        if "Autosave" not in core.config():
             core.config()["Autosave"] = {}
             core.config()["Autosave"]["Active"] = False
         return core.config()["Autosave"]["Active"]
