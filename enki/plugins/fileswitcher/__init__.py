45cfbe79e3c4eca202bea216a7e3c3b32f3487d4
Marco Laspe <marco@rockiger.com>
2017-09-28 11:18:12 +0200

    DONE Populate dialog, DONE Change file in dialog, DONE change file in enki

--- enki/plugins/fileswitcher/__init__.py
+++ enki/plugins/fileswitcher/__init__.py
@@ -10,9 +10,16 @@ Released under the GPL2 license
 https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 """
 # DONE Create dialog
-# TODO Populate dialog
-# TODO Change file in dialog
-# TODO change file in enki
+# DONE Populate dialog
+# DONE Change file in dialog
+# DONE change file in enki
+# TODO Change file backward
+# TODO change styling
+# TODO Setting page
+# TODO checkbox if autosave is activated,
+# TODO Cleanup code (Terminate plugin, etc.)
+
+from pprint import pprint
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QDialog, QTreeWidget, QTreeWidgetItem, QWidget, QCheckBox, QVBoxLayout,
@@ -32,23 +39,8 @@ class Plugin:
 
     def __init__(self):
         """pass"""
-        fileswitcher = QDialog(core.mainWindow())
-        fileswitcher.resize(400, 200)
-        fileswitcher.setModal(True)
-        fileswitcher.setWindowTitle("File Switcher")
-        vboxLayout = QVBoxLayout(fileswitcher)
-        filelist = QTreeWidget(fileswitcher)
-        filelist.setColumnCount(3)
-        filelist.setHeaderLabels(["Name", "Type", "Path"])
-        filelist.addTopLevelItem(QTreeWidgetItem(["File 1", "Type1", "Path 1"]))
-        filelist.addTopLevelItem(QTreeWidgetItem(("File 2", "Type2", "Path 2")))
-        filelist.addTopLevelItem(QTreeWidgetItem(("File 3", "Type3", "Path 3")))
-        filelist.setRootIsDecorated(False)
-
-        vboxLayout.addWidget(filelist)
+        self._fileswitcher = Fileswitcher(core.mainWindow())
         self._addAction()
-        self._fileSwitcher = fileswitcher
-
 
     def terminate(self):
         core.actionManager().removeAction(self._action)
@@ -60,8 +52,73 @@ class Plugin:
                                                 "File Switcher",
                                                 QIcon(':enkiicons/console.png'))
         core.actionManager().setDefaultShortcut(action, "Ctrl+Tab")
-        action.triggered.connect(self._showFileswitcher)
+        action.triggered.connect(self._fileswitcher._showFileswitcher)
         self._action = action
 
+class Fileswitcher(QDialog):
+    """docstring for Fileswitcher."""
+    def __init__(self, parent):
+        super(Fileswitcher, self).__init__(parent)
+        self._filestack = list()
+
+        self.resize(400, 200)
+        self.setModal(True)
+        self.setWindowTitle("File Switcher")
+        vboxLayout = QVBoxLayout(self)
+        filelist = QTreeWidget(self)
+        filelist.setColumnCount(3)
+        filelist.setHeaderLabels(["Name", "Type", "Path"])
+        filelist.setRootIsDecorated(False)
+        filelist.setAlternatingRowColors(True)
+
+        vboxLayout.addWidget(filelist)
+
+        core.workspace().documentOpened.connect(self._onDocumentOpened)
+        core.workspace().documentClosed.connect(self._onDocumentClosed)
+        core.workspace().currentDocumentChanged.connect(self._onDocumentChanged)
+
+        self._filelist = filelist
+
     def _showFileswitcher(self):
-        self._fileSwitcher.show()
+        self._filelist.clear()
+        for document in self._filestack:
+            self._filelist.addTopLevelItem(
+            QTreeWidgetItem((document.fileName(), "Type", document.filePath())))
+            # pprint(dir(document))
+        self._currentLine = 1
+        self._filelist.setCurrentItem(
+            self._filelist.topLevelItem(self._currentLine))
+        self.show()
+
+    def _onDocumentOpened(self, document):
+        self._filestack.insert(0, document)
+
+    def _onDocumentClosed(self, document):
+        idx = self._filestack.index(document)
+        self._filestack.pop(idx)
+
+    def _onDocumentChanged(self, old, new):
+        try:
+            idx = self._filestack.index(new)
+            self._filestack.pop(idx)
+            self._filestack.insert(0, new)
+        except ValueError as e:
+            print("new document couldn't be found")
+            print(e)
+
+    def keyPressEvent(self, event):
+        #super(Fileswitcher, self).keyPressEvent(event)
+        if (event.modifiers() & Qt.CTRL and event.key() == Qt.Key_Tab):
+            if self._currentLine < self._filelist.topLevelItemCount() - 1:
+                self._currentLine += 1
+            else:
+                self._currentLine = 0
+            self._filelist.setCurrentItem(
+                self._filelist.topLevelItem(self._currentLine))
+
+    def keyReleaseEvent(self, event):
+        if event.key() == Qt.Key_Control:
+            core.workspace().setCurrentDocument(
+                self._filestack[self._currentLine])
+            self._currentLine = None
+            self.hide()
