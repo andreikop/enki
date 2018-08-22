andreikop/enki

commit 66fd108f8502f5e44becc6f2a1ec45f0cda315f3

py3: 2to3

enki/core/actionmanager.py
@@ -72,7 +72,7 @@ class ActionManager(QObject):
     def del_(self):
         if self._pathToAction:
             assert 0, 'ActionManager: you have to delete all actions before destroying actions model. ' + \
-                      'Existing actions: ' + str(self._pathToAction.iterkeys())
+                      'Existing actions: ' + str(iter(self._pathToAction.keys()))
 
     def action(self, path ):
         """Get action by its path. i.e.
@@ -97,7 +97,7 @@ class ActionManager(QObject):
     def allActions(self):
         """Reqursive list of existing actions
         """
-        return self._pathToAction.itervalues()
+        return iter(self._pathToAction.values())
 
     @staticmethod
     def _parentPath(path):
@@ -113,7 +113,7 @@ class ActionManager(QObject):
         if parentAction is None:
             assert False, "Menu path not found: " + subPath
 
-        if isinstance(action, basestring):
+        if isinstance(action, str):
             action = QAction( icon, action, parentAction )
         else:
             action.setParent( parentAction )
@@ -178,7 +178,7 @@ class ActionManager(QObject):
         """Remove menu.
         If removeEmptyPath is True - remove also empty parent menus
         """
-        if isinstance(action, basestring):
+        if isinstance(action, str):
             action = self.action( action )
         assert action is not None
 
@@ -230,16 +230,16 @@ class ActionManager(QObject):
             return [object \
                         for object in QObject.children(self) \
                             if isinstance(object, QAction) and \
-                               object in self._pathToAction.itervalues()]
+                               object in iter(self._pathToAction.values())]
         else:
             return [object \
                         for object in action.children() \
-                            if object in self._pathToAction.itervalues()]
+                            if object in iter(self._pathToAction.values())]
 
     def defaultShortcut(self, action ):
         """Get actions default shortcut
         """
-        if isinstance(action, basestring):
+        if isinstance(action, str):
             action = self.action( action )
 
         if action is not None:
@@ -251,10 +251,10 @@ class ActionManager(QObject):
     def setDefaultShortcut(self, action, shortcut ):
         """Set actions default shortcut
         """
-        if isinstance(action, basestring):
+        if isinstance(action, str):
             action = self.action( action )
 
-        if isinstance(shortcut, basestring):
+        if isinstance(shortcut, str):
             shortcut = QKeySequence(shortcut)
 
         action.defaultShortcut = shortcut

bin/enki
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Enki. A text editor for programmers

enki/core/core.py
@@ -218,7 +218,7 @@ class Core(QObject):
                 shutil.move(_OLD_CONFIG_DIR, newPath)
             except Exception as ex:
                 text = 'Failed to move config directory from {} to {}: {}' \
-                    .format(_OLD_CONFIG_DIR, newPath, unicode(ex))
+                    .format(_OLD_CONFIG_DIR, newPath, str(ex))
                 QMessageBox.warning(None, 'Failed to move configs', text)
 
     def _createDefaultConfigFile(self):
@@ -231,13 +231,13 @@ class Core(QObject):
             try:
                 os.makedirs(enki.core.defines.CONFIG_DIR)
             except (OSError, IOError) as ex:
-                raise UserWarning(u'Failed to create directory "%s". Error: %s\n' % \
-                                  (enki.core.defines.CONFIG_DIR, unicode(str(ex), 'utf8')))
+                raise UserWarning('Failed to create directory "%s". Error: %s\n' % \
+                                  (enki.core.defines.CONFIG_DIR, str(str(ex), 'utf8')))
         try:
             shutil.copyfile(_DEFAULT_CONFIG_PATH, _CONFIG_PATH)
         except (OSError, IOError) as ex:
-            raise UserWarning(u'Failed to create configuration file "%s". Error:\n%s' % \
-                              (_CONFIG_PATH, unicode(str(ex), 'utf8')))
+            raise UserWarning('Failed to create configuration file "%s". Error:\n%s' % \
+                              (_CONFIG_PATH, str(str(ex), 'utf8')))
 
     def _createConfig(self):
         """Open main config file and return Config instance
@@ -255,7 +255,7 @@ class Core(QObject):
                 self._createDefaultConfigFile()
                 haveFileInHome = True
             except UserWarning as ex:
-                self.mainWindow().appendMessage(unicode(ex))
+                self.mainWindow().appendMessage(str(ex))
 
         # Try to open
         config = None

enki/core/document.py
@@ -250,15 +250,15 @@ class Document(QWidget):
         self._fileWatcher.setContents(data)
 
         try:
-            text = unicode(data, 'utf8')
-        except UnicodeDecodeError, ex:
+            text = str(data, 'utf8')
+        except UnicodeDecodeError as ex:
             QMessageBox.critical(None,
                                  self.tr("Can not decode file"),
                                  filePath + '\n' +
-                                 unicode(str(ex), 'utf8') +
+                                 str(str(ex), 'utf8') +
                                  '\nProbably invalid encoding was set. ' +
                                  'You may corrupt your file, if saved it')
-            text = unicode(data, 'utf8', 'replace')
+            text = str(data, 'utf8', 'replace')
 
         # Strip last EOL. Qutepart adds it when saving file
         if text.endswith('\r\n'):
@@ -333,8 +333,8 @@ class Document(QWidget):
         if  not os.path.exists(dirPath):
             try:
                 os.makedirs(dirPath)
-            except OSError, ex:
-                error = unicode(str(ex), 'utf8')
+            except OSError as ex:
+                error = str(str(ex), 'utf8')
                 QMessageBox.critical(None,
                                      self.tr("Cannot save file"),
                                      self.tr("Cannot create directory '%s'. Error '%s'." % (dirPath, error)))
@@ -353,7 +353,7 @@ class Document(QWidget):
         except IOError as ex:
             QMessageBox.critical(None,
                                  self.tr("Cannot write to file"),
-                                 unicode(str(ex), 'utf8'))
+                                 str(str(ex), 'utf8'))
             return
         finally:
             self._fileWatcher.enable()

enki/core/json_wrapper.py
@@ -27,14 +27,14 @@ def load(filePath, dataName, defaultValue):
         with open(filePath, 'r') as openedFile:
             try:
                 return json.load(openedFile)
-            except Exception, ex:  # broken file?
-                error = unicode(str(ex), 'utf8')
+            except Exception as ex:  # broken file?
+                error = str(str(ex), 'utf8')
                 text = "Failed to parse %s file '%s': %s" % (dataName, filePath, error)
                 core.mainWindow().appendMessage(text)
-                print >> sys.stderr, text
+                print(text, file=sys.stderr)
                 return defaultValue
-    except (OSError, IOError), ex:
-        error = unicode(str(ex), 'utf8')
+    except (OSError, IOError) as ex:
+        error = str(str(ex), 'utf8')
         text = "Failed to load %s file '%s': %s" % (dataName, filePath, error)
         core.mainWindow().appendMessage(text)
         return defaultValue
@@ -47,9 +47,9 @@ def dump(filePath, dataName, data, showWarnings=True):
     try:
         with open(filePath, 'w') as openedFile:
             json.dump(data, openedFile, sort_keys=True, indent=4)
-    except (OSError, IOError), ex:
-        error = unicode(str(ex), 'utf8')
+    except (OSError, IOError) as ex:
+        error = str(str(ex), 'utf8')
         text = "Failed to save %s to '%s': %s" % (dataName, filePath, error)
         if showWarnings and core.mainWindow() is not None:
             core.mainWindow().appendMessage(text)
-        print >> sys.stderr, error
+        print(error, file=sys.stderr)

enki/core/locator.py
@@ -12,7 +12,7 @@ from PyQt4.QtCore import pyqtSignal, QAbstractItemModel, QEvent, QModelIndex, QO
 from PyQt4.QtGui import QDialog, QFontMetrics, QLineEdit, QTreeView, QVBoxLayout
 
 from threading import Thread, Event
-from Queue import Queue
+from queue import Queue
 
 from enki.core.core import core
 from enki.lib.htmldelegate import HTMLDelegate
@@ -549,9 +549,9 @@ def splitLine(text):
     it = enumerate(text)
 
     def findNonSpace():
-        index, char = it.next()
+        index, char = next(it)
         while char.isspace():
-            index, char = it.next()
+            index, char = next(it)
 
         return index, char
 
@@ -564,7 +564,7 @@ def splitLine(text):
                     break
                 elif char == '\\':
                     try:
-                        index, char = it.next()
+                        index, char = next(it)
                     except StopIteration:
                         word += '\\'
                         raise
@@ -572,7 +572,7 @@ def splitLine(text):
                         word += char
                 else:
                     word += char
-                index, char = it.next()
+                index, char = next(it)
 
         except StopIteration:
             pass

enki/core/mainwindow.py
@@ -86,7 +86,7 @@ class MainWindow(QMainWindow):
     themselves.
     """  # pylint: disable=W0105
 
-    directoryDropt = pyqtSignal(unicode)
+    directoryDropt = pyqtSignal(str)
     """
     directoryDropt()
 
@@ -367,8 +367,8 @@ class MainWindow(QMainWindow):
         try:
             with open(path, 'wb') as f:
                 f.write(data)
-        except (OSError, IOError), ex:
-            error = unicode(str(ex), 'utf8')
+        except (OSError, IOError) as ex:
+            error = str(str(ex), 'utf8')
             QMessageBox.critical(None,
                                 self.tr("Cannot save {}".format(title)),
                                 self.tr("Cannot create file '%s'\nError: %s" % (path, error)))
@@ -380,8 +380,8 @@ class MainWindow(QMainWindow):
             try:
                 with open(path, 'rb') as f:
                     return f.read()
-            except (OSError, IOError), ex:
-                error = unicode(str(ex), 'utf8')
+            except (OSError, IOError) as ex:
+                error = str(str(ex), 'utf8')
                 QMessageBox.critical(None,
                                     self.tr("Cannot restore {}".format(title)),
                                     self.tr("Cannot read file '%s'\nError: %s" % (path, error)))

enki/core/project.py
@@ -19,8 +19,8 @@ STATUS_SHOW_TIMEOUT_MSEC = 3000
 
 
 class _ScannerThread(QThread):
-    itemsReady = pyqtSignal(unicode, list)
-    status = pyqtSignal(unicode)
+    itemsReady = pyqtSignal(str, list)
+    status = pyqtSignal(str)
 
     def __init__(self, parent, path):
         QThread.__init__(self, parent)
@@ -63,7 +63,7 @@ class _ScannerThread(QThread):
 
 class Project(QObject):
 
-    changed = pyqtSignal(unicode)
+    changed = pyqtSignal(str)
     """
     chagned(projectPath)
 
@@ -76,7 +76,7 @@ class Project(QObject):
 
     **Signal** emitted, when list of project files has been loaded
     """
-    scanStatusChanged = pyqtSignal(unicode)
+    scanStatusChanged = pyqtSignal(str)
     """
     scanStatusChanged()
 

commit 7921e6044e2cfcb0d8d0d93244dd463dfb3ddc0f

Fix print of action list on exception after 2to3
Fixes `AssertionError: ActionManager: you have to delete all actions before destroying
actions model. Existing actions: <dict_keyiterator object at 0x7f8512277598>`.

https://travis-ci.org/andreikop/enki/jobs/419067875#L845

The bug was introduced by 2to3 in 66fd108f8502.

enki/core/actionmanager.py
@@ -77,7 +77,7 @@ class ActionManager(QObject):
     def terminate(self):
         if self._pathToAction:
             assert 0, 'ActionManager: you have to delete all actions before destroying actions model. ' + \
-                      'Existing actions: ' + str(iter(self._pathToAction.keys()))
+                      'Existing actions: ' + ', '.join(self._pathToAction.keys())
 
     def action(self, path):
         """Get action by its path. i.e.